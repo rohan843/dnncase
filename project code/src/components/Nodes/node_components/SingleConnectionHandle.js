7ffecd9d26cc132fdbc22dd31bca0fabb5069591
rohan843 <rohan.sharma2934@gmail.com>
2023-08-28 00:18:12 +0530

    solve connection limit bug

--- project code/src/components/Nodes/node_components/SingleConnectionHandle.js
+++ project code/src/components/Nodes/node_components/SingleConnectionHandle.js
@@ -1,13 +1,26 @@
 import { Handle, getConnectedEdges, useNodeId, useStore } from "reactflow";
 
-// TODO: This has the HIGHEST priority.
-// FIXME: Modify this at line 8 to get the number of edges connected to the _handle_ not the node.
 function SingleConnectionHandle(props) {
   const enclosingNodeId = useNodeId();
   const isConnectable = useStore((s) => {
     const node = s.nodeInternals.get(enclosingNodeId);
     const connectedEdges = getConnectedEdges([node], s.edges);
-    return connectedEdges.length === 0;
+    const handleId = props.id;
+    const isCurHandleASource = props.type === "source";
+    console.log(connectedEdges);
+    return (
+      connectedEdges.filter((edge) => {
+        if (isCurHandleASource) {
+          return (
+            edge.sourceHandle === handleId && edge.source === enclosingNodeId
+          );
+        } else {
+          return (
+            edge.targetHandle === handleId && edge.target === enclosingNodeId
+          );
+        }
+      }).length === 0
+    );
   });
 
   return <Handle {...props} isConnectable={isConnectable} />;
