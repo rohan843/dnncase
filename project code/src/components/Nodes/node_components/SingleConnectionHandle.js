4e8669b19f2442d9ccbec37d3f8d48f0ff4128a6
rohan843 <rohan.sharma2934@gmail.com>
2023-08-27 23:21:16 +0530

    allow a max of 1 connection per handle

--- project code/src/components/Nodes/node_components/SingleConnectionHandle.js
+++ project code/src/components/Nodes/node_components/SingleConnectionHandle.js
@@ -1,7 +1,14 @@
-import { Handle } from "reactflow";
+import { Handle, getConnectedEdges, useNodeId, useStore } from "reactflow";
 
 function SingleConnectionHandle(props) {
-  return <Handle {...props} />;
+  const enclosingNodeId = useNodeId();
+  const isConnectable = useStore((s) => {
+    const node = s.nodeInternals.get(enclosingNodeId);
+    const connectedEdges = getConnectedEdges([node], s.edges);
+    return connectedEdges.length === 0;
+  });
+
+  return <Handle {...props} isConnectable={isConnectable} />;
 }
 
 export default SingleConnectionHandle;
