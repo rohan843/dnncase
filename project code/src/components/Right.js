e1f9d722b82e4bc82c1a4ef23876d40912732a7e
Rohan_Ray123 <rohanray200@gmail.com>
2023-09-06 17:56:26 +0530

    small fix - edge case

--- project code/src/components/Right.js
+++ project code/src/components/Right.js
@@ -8,6 +8,12 @@ function Right(props) {
   
   const nodeToDisplay = props.nodes.find(node => node.id === props.text);
 
+  if(!nodeToDisplay){
+    return <div style={{height: "87.5vh", width: "15vw", border: "1px solid black"}}>
+      No Node selected
+      </div>;
+  }
+
   return <div style={{height: "87.5vh", width: "15vw", border: "1px solid black"}}>
     Right
     <div className="info">HyperParameters : {props.text}</div>
