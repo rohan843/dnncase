f14b5bfd5991fa0a575c2a52509fb58c288aa16e
rohan843 <rohan.sharma2934@gmail.com>
2023-10-30 17:18:20 +0530

    update

--- system-code/electron/main.js
+++ system-code/electron/main.js
@@ -2,9 +2,7 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow();
-
   win.maximize();
-
   win.loadURL("http://localhost:3000");
 };
 
