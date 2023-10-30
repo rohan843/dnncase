e28f4b518f49205987cf17e19c46050d3bb29916
rohan843 <rohan.sharma2934@gmail.com>
2023-10-30 17:58:23 +0530

    import tailwind

--- system-code/src/components/topBarComponents/Logo.js
+++ system-code/src/components/topBarComponents/Logo.js
@@ -1,11 +1,7 @@
 import { ReactComponent as LogoSVG } from "../../assets/Logo.svg";
 
 function Logo() {
-  return (
-    <h1>
-      <LogoSVG />
-    </h1>
-  );
+  return <LogoSVG />;
 }
 
 export default Logo;
