52303b8d36050b4b62cdfdb87ab1411605e2d640
rohan843 <rohan.sharma2934@gmail.com>
2023-11-02 00:38:04 +0530

    minor update

--- system-code/src/components/mainScreenComponents/terminal/Terminal.js
+++ system-code/src/components/mainScreenComponents/terminal/Terminal.js
@@ -1,6 +1,13 @@
 function Terminal() {
   return (
     <div className="border-top-darker w-full h-20 background-dark z-10 absolute bottom-0">
+      <div
+        className="w-full h-[2px] -top-[1px] relative cursor-row-resize z-10"
+        onDrag={(e) => {
+          console.log(e);
+        }}
+        draggable="true"
+      />
       Terminal
     </div>
   );
