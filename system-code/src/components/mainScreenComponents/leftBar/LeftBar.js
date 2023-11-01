904af2aeb1e4a98c85287dbb89398c0848929165
rohan843 <rohan.sharma2934@gmail.com>
2023-11-02 00:04:43 +0530

    border corection++

--- system-code/src/components/mainScreenComponents/leftBar/LeftBar.js
+++ system-code/src/components/mainScreenComponents/leftBar/LeftBar.js
@@ -2,7 +2,7 @@ import OptionsList from "./OptionsList";
 
 function LeftBar() {
   return (
-    <div className="border-darker p-px h-full background-dark overflow-y-auto hide-scrollbar">
+    <div className="border-right-darker p-px h-full background-dark overflow-y-auto hide-scrollbar">
       <OptionsList />
     </div>
   );
