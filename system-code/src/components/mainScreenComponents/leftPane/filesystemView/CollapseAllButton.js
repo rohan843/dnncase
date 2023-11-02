524fc979dc83ade6c0ca874862834879d8ed047e
rohan843 <rohan.sharma2934@gmail.com>
2023-11-02 23:42:47 +0530

    add tooltips

--- system-code/src/components/mainScreenComponents/leftPane/filesystemView/CollapseAllButton.js
+++ system-code/src/components/mainScreenComponents/leftPane/filesystemView/CollapseAllButton.js
@@ -1,10 +1,13 @@
 import collapseImage from "../../../../assets/collapse.png";
+import ToolTipWrapper from "../../../tooltipWrapper/ToolTipWrapper";
 
 function CollapseAllButton() {
   return (
-    <div className="h-full py-2 px-1 opacity-50 hover:opacity-100 cursor-pointer">
-      <img src={collapseImage} alt="" className="h-full" />
-    </div>
+    <ToolTipWrapper className="h-full" helpText="Collapse All Folders">
+      <div className="h-full py-2 px-1 opacity-50 hover:opacity-100 cursor-pointer">
+        <img src={collapseImage} alt="" className="h-full" />
+      </div>
+    </ToolTipWrapper>
   );
 }
 
--- system-code/src/components/mainScreenComponents/leftPane/filesystemView/RefreshButton.js
+++ system-code/src/components/mainScreenComponents/leftPane/filesystemView/RefreshButton.js
@@ -1,11 +1,14 @@
 import refreshImage from "../../../../assets/refresh.png";
+import ToolTipWrapper from "../../../tooltipWrapper/ToolTipWrapper";
 
 function RefreshButton() {
-    return (
-        <div className="h-full py-2 px-1 opacity-50 hover:opacity-100 cursor-pointer">
-          <img src={refreshImage} alt="" className="h-full" />
-        </div>
-      );
+  return (
+    <ToolTipWrapper className="h-full" helpText="Refresh Filesystem Data">
+      <div className="h-full py-2 px-1 opacity-50 hover:opacity-100 cursor-pointer">
+        <img src={refreshImage} alt="" className="h-full" />
+      </div>
+    </ToolTipWrapper>
+  );
 }
 
 export default RefreshButton;
