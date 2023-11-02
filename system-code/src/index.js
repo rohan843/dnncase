de37d5e10d363e61281500098a414db6a2160f39
rohan843 <rohan.sharma2934@gmail.com>
2023-11-03 03:47:08 +0530

    include necessary classes

--- system-code/src/index.js
+++ system-code/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import 'react-tooltip/dist/react-tooltip.css'
+import "react-tooltip/dist/react-tooltip.css";
+import "react-complex-tree/lib/style-modern.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
