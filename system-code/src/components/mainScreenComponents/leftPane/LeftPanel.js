3299ae5206a8b15eb10a1b61ca794af17e61307a
rohan843 <rohan.sharma2934@gmail.com>
2023-11-02 17:21:14 +0530

    update width for development

--- system-code/src/components/mainScreenComponents/leftPane/LeftPanel.js
+++ system-code/src/components/mainScreenComponents/leftPane/LeftPanel.js
@@ -1,5 +1,5 @@
 function LeftPanel() {
-  return <div className="border-right-darker h-full background-light">LeftPanel</div>;
+  return <div className="border-right-darker h-full background-light w-[30%]">LeftPanel</div>;
 }
 
 export default LeftPanel;
