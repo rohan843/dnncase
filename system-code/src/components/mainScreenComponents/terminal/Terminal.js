c6bc6b5f95cc77440754448682ce15f2840cc4ef
rohan843 <rohan.sharma2934@gmail.com>
2023-11-02 17:12:48 +0530

    update metadata

--- system-code/src/components/mainScreenComponents/terminal/Terminal.js
+++ system-code/src/components/mainScreenComponents/terminal/Terminal.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
+// TODO: See -> https://stackoverflow.com/questions/26233180/resize-a-div-on-border-drag-and-drop-without-adding-extra-markup
 function Terminal() {
+  const [height, setHeight] = useState(300);
   return (
-    <div className="border-top-darker w-full h-20 background-dark z-10 absolute bottom-0">
+    <div
+      className="border-top-darker w-full background-dark z-10 absolute bottom-0"
+      style={{ height: `${height}px` }}
+    >
       <div
         className="w-full h-[2px] -top-[1px] relative cursor-row-resize z-10"
         onDrag={(e) => {
-          console.log(e);
+          console.log(e.nativeEvent.offsetY);
+          // setHeight(height + -1 * e.nativeEvent.offsetY);
         }}
         draggable="true"
       />
--- system-code/src/components/mainScreenComponents/workspace/Workspace.js
+++ system-code/src/components/mainScreenComponents/workspace/Workspace.js
@@ -1,5 +1,5 @@
 function Workspace() {
-  return <div className="border-2 w-full h-full z-0">Workspace</div>;
+  return <div className="w-full h-full z-0">Workspace</div>;
 }
 
 export default Workspace;
