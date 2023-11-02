f23d77bf6d90fe0d128dfcc1690a6186b7622d7b
rohan843 <rohan.sharma2934@gmail.com>
2023-11-02 23:54:40 +0530

    take value from global constants file

--- system-code/src/components/tooltipWrapper/HelpTooltip.js
+++ system-code/src/components/tooltipWrapper/HelpTooltip.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useId } from "react";
 import { Tooltip } from "react-tooltip";
+import { ToolTipDelays } from "../../constants";
 
 function HelpTooltip({ children, helpText, customDelayMS, ...props }) {
   const id = useId();
@@ -9,7 +10,11 @@ function HelpTooltip({ children, helpText, customDelayMS, ...props }) {
       <a data-tooltip-id={id} data-tooltip-content={helpText}>
         {children}
       </a>
-      <Tooltip id={id} delayShow={customDelayMS || 100} className="z-50" />
+      <Tooltip
+        id={id}
+        delayShow={customDelayMS || ToolTipDelays.small}
+        className="z-50"
+      />
     </div>
   );
 }
