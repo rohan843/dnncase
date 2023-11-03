d2142c08bc93dea4e45f26155597ced66cb252c7
rohan843 <rohan.sharma2934@gmail.com>
2023-11-03 16:40:54 +0530

    updates

--- system-code/src/index.js
+++ system-code/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import "./styles/index.css";
 import "react-tooltip/dist/react-tooltip.css";
 import "react-complex-tree/lib/style-modern.css";
@@ -8,6 +11,8 @@ import "react-complex-tree/lib/style-modern.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
