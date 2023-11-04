4fd935e3c82cfd5336bc7fc5e2934af4797df9e0
rohan843 <rohan.sharma2934@gmail.com>
2023-11-05 03:19:39 +0530

    start with highlight button

--- system-code/src/components/mainScreenComponents/terminal/terminalContents/logsTerminalContent/LogMessage.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalContents/logsTerminalContent/LogMessage.js
@@ -2,12 +2,13 @@ import MessageTimestamp from "./MessageTimestamp";
 import MessageString from "./MessageString";
 import classNames from "classnames";
 import { logSeverities } from "../../../../../constants";
+import highlightTextImage from "../../../../../assets/highlight-text.png";
 
 function LogMessage({ timestamp, logString, logSource, logSeverity }) {
   return (
     <div
       className={classNames(
-        "w-full hover-border-black-else-dark hover-background-slightly-darker rounded h-max flex flex-row",
+        "w-full hover-border-black-else-dark hover-background-slightly-darker rounded h-max flex flex-row relative",
         {
           "text-rose-800": logSeverity === logSeverities.error,
           "text-yellow-800": logSeverity === logSeverities.warning,
@@ -20,6 +21,9 @@ function LogMessage({ timestamp, logString, logSource, logSeverity }) {
         logSource={logSource}
         logSeverity={logSeverity}
       />
+      <div className="h-5 w-5 absolute right-2 z-30 opacity-20 hover:opacity-50 cursor-pointer mt-auto mb-auto top-0 bottom-0">
+        <img src={highlightTextImage} alt="" />
+      </div>
     </div>
   );
 }
