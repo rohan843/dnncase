440316adf239001a10f2fec3f7ff9db32ba17946
rohan843 <rohan.sharma2934@gmail.com>
2023-11-04 18:43:34 +0530

    add comments terminal controls

--- system-code/src/components/mainScreenComponents/terminal/terminalNav/CommentsTerminalControls.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalNav/CommentsTerminalControls.js
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import FilterBox from "./FilterBox";
+import { setCommentsFilterString } from "../../../../store";
+
 function CommentsTerminalControls() {
-  return <div>CommentsTerminalControls</div>;
+  const dispatch = useDispatch();
+  const { filterString } = useSelector((state) => state.commentsTerminal);
+
+  const handleFilterStringChange = (newFilterString) => {
+    dispatch(setCommentsFilterString(newFilterString));
+  };
+
+  return (
+    <div className="flex flex-row items-center">
+      <FilterBox
+        value={filterString}
+        onValueChange={handleFilterStringChange}
+      />
+    </div>
+  );
 }
 
 export default CommentsTerminalControls;
