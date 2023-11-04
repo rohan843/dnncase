ac078b05de53b985fd85b168854eb9b4400a1e22
rohan843 <rohan.sharma2934@gmail.com>
2023-11-05 01:03:52 +0530

    create terminal content container

--- system-code/src/components/mainScreenComponents/terminal/terminalContents/TerminalContent.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalContents/TerminalContent.js
@@ -1,8 +1,12 @@
+// This component guarantees vertical scrolling, so its children can assume infinite height
+// However, any children must be responsive to width (i.e., x-direction). There is only a
+// finite width available and any x-overflows are hidden. In case of text wrapping, consider
+// the following CSS styles to allow hyphenation (hyphens) and automatic work break (overflow-wrap):
+//      hyphens: auto;              -> break-and-wrap-large-words
+//      overflow-wrap: anywhere;    -> hyphenate-word-breaks-automatically
 function TerminalContent() {
   return (
-    <div className="w-full terminal-content-height">
-      TerminalContent
-    </div>
+    <div className="w-full terminal-content-height pl-1 pr-2 pt-1 overflow-y-auto overflow-x-hidden stable-scrollbar-gutter style-scrollbar-rounded-no-buttons"></div>
   );
 }
 
