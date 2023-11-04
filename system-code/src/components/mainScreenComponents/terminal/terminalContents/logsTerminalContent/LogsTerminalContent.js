f5ff802840c8b658639ad26edba02e4c5dc79c00
rohan843 <rohan.sharma2934@gmail.com>
2023-11-05 01:52:04 +0530

    finish basic logs view

--- system-code/src/components/mainScreenComponents/terminal/terminalContents/logsTerminalContent/LogsTerminalContent.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalContents/logsTerminalContent/LogsTerminalContent.js
@@ -1,5 +1,71 @@
 function LogsTerminalContent() {
-  return <div>LogsTerminalContent</div>;
+  return (
+    <div className="flex flex-col pb-2 dark-text-selection">
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+      <div className="w-full hover-border-black-else-dark rounded h-max flex flex-row">
+        <div className="font-mono w-[10rem]">[17/9/2023 20:15]</div>
+        <div className="w-[calc(100%-10rem)]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          corrupti molestiae aliquam quo modi similique expedita sequi
+          reprehenderit, sit eaque consequuntur rerum illo soluta ipsum nihil
+          magni atque quas totam?
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LogsTerminalContent;
