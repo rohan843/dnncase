8ba810926c808eebdb40ff3d799bee2579b85bca
rohan843 <rohan.sharma2934@gmail.com>
2023-11-04 18:41:45 +0530

    add validation terminal controls

--- system-code/src/components/mainScreenComponents/terminal/terminalNav/ValidationTerminalControls.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalNav/ValidationTerminalControls.js
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import FilterBox from "./FilterBox";
+import { setValidationFilterString } from "../../../../store";
+
 function ValidationTerminalControls() {
-  return <div>ValidationTerminalControls</div>;
+  const dispatch = useDispatch();
+  const { filterString } = useSelector((state) => state.validationTerminal);
+
+  const handleFilterStringChange = (newFilterString) => {
+    dispatch(setValidationFilterString(newFilterString));
+  };
+
+  return (
+    <div className="flex flex-row items-center">
+      <FilterBox
+        value={filterString}
+        onValueChange={handleFilterStringChange}
+      />
+    </div>
+  );
 }
 
 export default ValidationTerminalControls;
