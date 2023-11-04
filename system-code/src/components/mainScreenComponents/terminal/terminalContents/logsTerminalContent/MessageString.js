aa343610b7713610cac085fbdfcbe8d594f3992d
rohan843 <rohan.sharma2934@gmail.com>
2023-11-05 02:34:31 +0530

    add comments

--- system-code/src/components/mainScreenComponents/terminal/terminalContents/logsTerminalContent/MessageString.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalContents/logsTerminalContent/MessageString.js
@@ -1,4 +1,12 @@
+// This component is capable of displaying any log message string. It supports
+// common escape sequences such as tabs (\t) and newlines (\n). Note that tabs
+// are treated as a larger white space and any table-like tab based effects may
+// not be easy to produce.
 function MessageString({ logString }) {
-  return <div className="w-[calc(100%-11rem)] whitespace-break-spaces">{logString}</div>;
+  return (
+    <div className="w-[calc(100%-11rem)] whitespace-break-spaces">
+      {logString}
+    </div>
+  );
 }
 export default MessageString;
