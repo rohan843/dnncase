b4d4189201698a7b1ec8e8625a648374753078ce
rohan843 <rohan.sharma2934@gmail.com>
2023-11-06 02:08:05 +0530

    more formatting

--- system-code/src/index.js
+++ system-code/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import "./styles/unreset.scss";
 
 import "./styles/index.css";
 import "react-tooltip/dist/react-tooltip.css";
 import "react-complex-tree/lib/style-modern.css";
+
+import "./styles/unreset.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
