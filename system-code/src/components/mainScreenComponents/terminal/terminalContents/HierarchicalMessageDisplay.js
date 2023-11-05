077c1c9bb2a7468bab3c6e33c58ab3a53870dcc1
rohan843 <rohan.sharma2934@gmail.com>
2023-11-05 18:59:16 +0530

    update to code

--- system-code/src/components/mainScreenComponents/terminal/terminalContents/HierarchicalMessageDisplay.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalContents/HierarchicalMessageDisplay.js
@@ -1,7 +1,24 @@
-import { useState } from "react";
-import { ControlledTreeEnvironment, Tree } from "react-complex-tree";
+import {
+  StaticTreeDataProvider,
+  Tree,
+  UncontrolledTreeEnvironment,
+} from "react-complex-tree";
+
+import openDropdownIcon from "../../../../assets/hierarchy-dropdown-open.png";
+import closedDropdownIcon from "../../../../assets/hierarchy-dropdown-close.png";
+import errorIcon from "../../../../assets/error.png";
+import warningIcon from "../../../../assets/warning.png";
+import infoIcon from "../../../../assets/info.png";
 
 const dummyMessages = {
+  root: {
+    index: "root",
+    children: [
+      "/Project1/home/user1/file1.txt",
+      "/Project1/home/user1/file2.txt",
+    ],
+    isFolder: true,
+  },
   "/Project1/home/user1/file1.txt": {
     index: "/Project1/home/user1/file1.txt",
     data: { name: "file1.txt", type: "file" },
@@ -9,6 +26,7 @@ const dummyMessages = {
       "/Project1/home/user1/file1.txt/errors/0",
       "/Project1/home/user1/file1.txt/errors/1",
     ],
+    isFolder: true,
   },
   "/Project1/home/user1/file1.txt/errors/0": {
     index: "/Project1/home/user1/file1.txt/errors/0",
@@ -43,6 +61,7 @@ const dummyMessages = {
       "/Project1/home/user1/file2.txt/warnings/0",
       "/Project1/home/user1/file2.txt/infos/0",
     ],
+    isFolder: true,
   },
   "/Project1/home/user1/file2.txt/warnings/0": {
     index: "/Project1/home/user1/file2.txt/warnings/0",
@@ -71,46 +90,116 @@ const dummyMessages = {
 };
 
 function HierarchicalMessageDisplay() {
-  const [selectedItems, setSelectedItems] = useState([]);
   return (
     <div className="w-full h-max">
-      <ControlledTreeEnvironment
-        items={dummyMessages}
-        getItemTitle={() => ""}
+      <UncontrolledTreeEnvironment
+        dataProvider={
+          new StaticTreeDataProvider(dummyMessages, (item, data) => ({
+            ...item,
+            data,
+          }))
+        }
+        getItemTitle={(item) => item.data.type === "file" && item.data.name}
         viewState={{
-          "msg-tree": {
-            selectedItems,
-          },
+          "tree-1": {},
         }}
-        canDragAndDrop={false}
-        canDropOnFolder={false}
-        canReorderItems={false}
-        onExpandItem={(item) => {
-          setSelectedItems([...selectedItems, item.index]);
+        renderItem={({ title, context, children, item }) => {
+          if (item.data.type === "file") {
+            return (
+              <div
+                {...context.itemContainerWithChildrenProps}
+                className="m-0 flex flex-col mb-1"
+              >
+                <div
+                  {...context.itemContainerWithoutChildrenProps}
+                  {...context.interactiveElementProps}
+                  className="flex flex-row items-center mb-1"
+                >
+                  {/* Arrow */}
+                  <div
+                    {...context.arrowProps}
+                    className="h-full flex items-center"
+                  >
+                    {context.isExpanded ? (
+                      <img src={openDropdownIcon} alt="" className="h-1 mx-1" />
+                    ) : (
+                      <img
+                        src={closedDropdownIcon}
+                        alt=""
+                        className="w-1 mx-1"
+                      />
+                    )}
+                  </div>
+                  {/* Title */}
+                  <span>{title}</span>
+                </div>
+                <div className="w-full h-max ml-1.5">{children}</div>
+              </div>
+            );
+          } else {
+            return (
+              <div
+                {...context.itemContainerWithChildrenProps}
+                className="m-0 flex flex-col mb-1"
+              >
+                <div
+                  {...context.itemContainerWithoutChildrenProps}
+                  {...context.interactiveElementProps}
+                >
+                  {/* Message Div */}
+                  <div className="flex flex-row items-center mb-1">
+                    {/* Arrow */}
+                    <div
+                      {...context.arrowProps}
+                      className="h-full flex items-center"
+                    >
+                      <img
+                        src={
+                          (item.data.messageContent.type === "error" &&
+                            errorIcon) ||
+                          (item.data.messageContent.type === "warning" &&
+                            warningIcon) ||
+                          (item.data.messageContent.type === "info" && infoIcon)
+                        }
+                        alt=""
+                        className="h-5 w-5 mx-1"
+                      />
+                    </div>
+                    {/* Message */}
+                    <div className="ml-1 whitespace-break-spaces">
+                      <span>{item.data.messageContent.message}</span>
+                      <span>
+                        {item.data.messageContent.affectedNodes &&
+                          item.data.messageContent.affectedNodes.join(" ")}
+                      </span>
+                      <span>
+                        {item.data.messageContent.displayPositionCoordinates &&
+                          `pos=(${item.data.messageContent.displayPositionCoordinates[0]},${item.data.messageContent.displayPositionCoordinates[1]})`}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          }
         }}
-        onCollapseItem={(item) => {
-          setSelectedItems(
-            selectedItems.filter(
-              (selectedItemIndex) => selectedItemIndex !== item.index
-            )
+        renderItemsContainer={({ children, containerProps }) => {
+          return (
+            <div {...containerProps} className="flex flex-col mr-2 ml-1">
+              {children}
+            </div>
           );
         }}
-        renderItemTitle={() => {}}
-        renderItemArrow={() => {}}
-        renderItem={() => {}}
-        renderItemsContainer={() => {}}
         renderTreeContainer={({ children, containerProps }) => {
-          <div {...containerProps} className="">
-            {children}
-          </div>;
+          return (
+            <div {...containerProps} className="w-full h-max">
+              {children}
+            </div>
+          );
         }}
       >
-        <Tree
-          treeId="msg-tree"
-          treeLabel="File Specific Messages"
-          rootItem=""
-        />
-      </ControlledTreeEnvironment>
+        <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+      </UncontrolledTreeEnvironment>
     </div>
   );
 }
