ebb8c4f5140fed4d2f5e78f43b3924b9dad017b0
rohan843 <rohan.sharma2934@gmail.com>
2023-11-06 00:26:09 +0530

    setup comments terminal content

--- system-code/src/components/mainScreenComponents/terminal/terminalContents/CommentsTerminalContent.js
+++ system-code/src/components/mainScreenComponents/terminal/terminalContents/CommentsTerminalContent.js
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
 import HierarchicalMessageDisplay from "./HierarchicalMessageDisplay";
+import {
+  addExpandedCommentItem,
+  removeExpandedCommentItem,
+} from "../../../../store";
+import CommentsMessageDisplay from "./CommentsMessageDisplay";
 
 function CommentsTerminalContent() {
+  const { messages, expandedItems } = useSelector(
+    (store) => store.commentsTerminal
+  );
   return (
     <div className="w-full h-max">
-      <HierarchicalMessageDisplay />
+      <HierarchicalMessageDisplay
+        messages={messages}
+        expandedItems={expandedItems}
+        addExpandedItem={addExpandedCommentItem}
+        removeExpandedItem={removeExpandedCommentItem}
+        MessageDisplay={CommentsMessageDisplay}
+      />
     </div>
   );
 }
