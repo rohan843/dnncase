6bdfa82406ad2c3e9eb8ca8690247377447aa09d
rohan843 <rohan.sharma2934@gmail.com>
2023-11-06 02:27:30 +0530

    add comments

--- system-code/src/components/utils/MarkdownTextDisplay.js
+++ system-code/src/components/utils/MarkdownTextDisplay.js
@@ -1,6 +1,10 @@
+// TODO: See this: https://github.com/remarkjs/react-markdown#appendix-b-components and add individual react components, instead of using unreset.
+
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+// Takes in a markdown string and returns appropriate JSX. GFM (GitHub
+// Flavoured Markdown) is supported.
 function MarkdownTextDisplay({ mdString }) {
   return (
     <span className="this_is_dangerous-unreset-only_for_markdown_use">
