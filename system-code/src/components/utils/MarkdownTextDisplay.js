e91a2a24eff92b0f4132212fb8c194cff9924b4c
rohan843 <rohan.sharma2934@gmail.com>
2023-11-06 01:44:07 +0530

    TODO: configure thisisdangerous-unreset class

--- system-code/src/components/utils/MarkdownTextDisplay.js
+++ system-code/src/components/utils/MarkdownTextDisplay.js
@@ -1,5 +1,14 @@
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 function MarkdownTextDisplay({ mdString }) {
-  return <span>{mdString}</span>;
+  return (
+    <div>
+      <span className="thisisdangerous-unreset">
+        <Markdown remarkPlugins={[remarkGfm]}>{mdString}</Markdown>
+      </span>
+    </div>
+  );
 }
 
 export default MarkdownTextDisplay;
