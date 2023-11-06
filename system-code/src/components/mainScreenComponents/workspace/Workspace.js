cc923376bc1708364aa287928f34877dc2aea789
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 03:13:58 +0530

    add quick access bar

--- system-code/src/components/mainScreenComponents/workspace/Workspace.js
+++ system-code/src/components/mainScreenComponents/workspace/Workspace.js
@@ -1,9 +1,17 @@
+import QuickAddressBar from "./topBars/QuickAddressBar";
 import TopTabBar from "./topBars/TopTabBar";
+
+const dummyAddressList = [
+  { name: "home1", onClick: () => {} },
+  { name: "user1", onClick: () => {} },
+  { name: "file1.txt", onClick: () => {} },
+];
 
 function Workspace() {
   return (
     <div className="w-full h-full z-0">
       <TopTabBar />
+      <QuickAddressBar addressList={dummyAddressList} />
     </div>
   );
 }
---
+++ system-code/src/components/mainScreenComponents/workspace/topBars/QuickAddressBar.js
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+import useHorizontalScrolling from "../../../../hooks/useHorizontalScroll";
+
+function QuickAddressBar({ addressList }) {
+  const ref = useRef(null);
+  const handleScroll = useHorizontalScrolling(ref);
+  const addressComponents = addressList.map((addressComponent, idx) => {
+    return (
+      <span
+        className="h-full w-max text-gray-600 hover:text-gray-950 select-none cursor-pointer"
+        onClick={addressComponent.onClick}
+        key={idx}
+        onWheel={handleScroll}
+      >
+        {addressComponent.name}
+        {idx < addressList.length - 1 && (<span className="px-1">&gt;</span>)}
+      </span>
+    );
+  });
+  return (
+    <div
+      ref={ref}
+      className="h-6 flex flex-row items-center pl-1 pr-5 w-full border-bottom-darker background-dark overflow-x-scroll overflow-y-hidden hide-scrollbar"
+    >
+      {addressComponents}
+    </div>
+  );
+}
+export default QuickAddressBar;
