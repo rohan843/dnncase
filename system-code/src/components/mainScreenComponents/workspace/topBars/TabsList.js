901f67fc98d2bda37d3cfd297c461d719c9e1e4c
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 01:40:28 +0530

    create and use horizontal scrolling hook

--- system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
@@ -1,16 +1,9 @@
 import { useRef } from "react";
+import useHorizontalScrolling from "../../../../hooks/useHorizontalScroll";
 
 function TabsList() {
   const ref = useRef(null);
-  const handleScroll = (event) => {
-    const container = ref.current;
-    const scrollAmount = event.deltaY;
-    container.scrollTo({
-      top: 0,
-      left: container.scrollLeft + scrollAmount * 1.3,
-      behavior: "instant",
-    });
-  };
+  const handleScroll = useHorizontalScrolling(ref);
   return (
     <div
       className="h-full min-w-[10%] border grow select-none overflow-x-scroll overflow-y-clip hide-scrollbar flex flex-row items-center"
