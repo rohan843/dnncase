ae0ab165a09907486ffca9aa90f5a85383b42b8d
rohan843 <rohan.sharma2934@gmail.com>
2023-11-06 22:45:00 +0530

    add comments

--- system-code/src/components/activationAndKeystrokeMonitoring/ActivatableEntity.js
+++ system-code/src/components/activationAndKeystrokeMonitoring/ActivatableEntity.js
@@ -2,7 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { setActiveElementTo } from "../../store";
 import { useEffect, useRef } from "react";
 
-function ActivatableEntity({ id, children }) {
+// This component wraps around some JSX elements and any time a click is detected
+// on any of those elements, the provided ID is set as the active element's ID.
+// (Repeated clicks on the same ActivatableEntity _donot_ trigger new dispatches;
+// a dispatch is only triggered when the active ID and the provided ID are unequal.)
+function ActivatableEntity({ id, children, ...props }) {
   const ref = useRef(null);
   const dispatch = useDispatch();
   const { id: activeElementID } = useSelector((store) => store.activeElement);
@@ -13,13 +17,17 @@ function ActivatableEntity({ id, children }) {
         dispatch(setActiveElementTo(id));
       }
     };
-    ref.addEventListener("click", handleClick);
+    ref.addEventListener("click", handleClick, { capture: true });
     return () => {
       ref.removeEventListener("click", handleClick);
     };
   }, [activeElementID, dispatch, id]);
 
-  return <div ref={ref}>{children}</div>;
+  return (
+    <div ref={ref} {...props}>
+      {children}
+    </div>
+  );
 }
 
 export default ActivatableEntity;
