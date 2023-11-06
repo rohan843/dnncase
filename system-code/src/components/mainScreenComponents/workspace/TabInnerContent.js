216eee1aa79adeefe2345e4d065d27e9cb5aa3aa
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 03:39:44 +0530

    remove padding

--- system-code/src/components/mainScreenComponents/workspace/TabInnerContent.js
+++ system-code/src/components/mainScreenComponents/workspace/TabInnerContent.js
@@ -2,7 +2,7 @@ import ModelCanvas from "./fileTypeSpecificTabs/ModelCanvas/ModelCanvas";
 
 function TabInnerContent() {
   return (
-    <div style={{ height: "calc(100% - 4rem)" }} className="w-full p-2">
+    <div style={{ height: "calc(100% - 4rem)" }} className="w-full">
       <ModelCanvas />
     </div>
   );
