0b7202d6d6c5ef32a50196999f4f66f4fc1262fd
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 04:20:25 +0530

    set dimensions

--- system-code/src/components/topBarComponents/Logo.js
+++ system-code/src/components/topBarComponents/Logo.js
@@ -2,7 +2,7 @@ import logoImage from "../../assets/Logo.png";
 
 function Logo() {
   return (
-    <div className="pl-2">
+    <div className="pl-2 h-8 w-max">
       <img className="h-8" src={logoImage} alt="" />
     </div>
   );
