cf0eb0189c0840325f5457467909e55344a9d40b
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 03:39:01 +0530

    start with model canvas

--- system-code/src/components/mainScreenComponents/workspace/TabInnerContent.js
+++ system-code/src/components/mainScreenComponents/workspace/TabInnerContent.js
@@ -1,7 +1,9 @@
+import ModelCanvas from "./fileTypeSpecificTabs/ModelCanvas/ModelCanvas";
+
 function TabInnerContent() {
   return (
-    <div style={{height: "calc(100% - 4rem)"}} className="w-full p-2">
-      TabInnerContent
+    <div style={{ height: "calc(100% - 4rem)" }} className="w-full p-2">
+      <ModelCanvas />
     </div>
   );
 }
---
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ModelCanvas.js
@@ -0,0 +1,5 @@
+function ModelCanvas() {
+  return <div className="h-full w-full">asdf</div>;
+}
+
+export default ModelCanvas;
