faa392ac39e4199c0a2baecbff667092b94b392c
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 02:50:52 +0530

    add padding

--- system-code/src/components/mainScreenComponents/workspace/topBars/fileTypeSpecificOptions/OptionButtonStyling.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/fileTypeSpecificOptions/OptionButtonStyling.js
@@ -1,6 +1,6 @@
 function OptionButtonStyling({ iconPNG }) {
   return (
-    <div className="h-8 w-8 flex items-center justify-center cursor-pointer rounded hover-background-darker">
+    <div className="h-8 w-8 py-1 flex items-center justify-center cursor-pointer rounded hover-background-darker">
       <img src={iconPNG} alt="" className="h-6" />
     </div>
   );
