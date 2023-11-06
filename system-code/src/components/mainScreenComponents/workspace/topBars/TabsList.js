214daa68154830bbd5de0d4c0a8f4ce7f98ffafc
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 00:50:44 +0530

    crash occurred: remove react-horizontal scroll

--- system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
@@ -1,36 +1,36 @@
 function TabsList() {
   return (
-    <div className="h-full min-w-[10%] grow border overflow-x-scroll overflow-y-clip hide-scrollbar flex flex-row">
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
-        <span>TabsList</span>
+    <div className="h-full min-w-[10%] border grow select-none overflow-x-scroll overflow-y-clip hide-scrollbar flex flex-row items-center">
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
+      <span>TabsList</span>
     </div>
   );
 }
