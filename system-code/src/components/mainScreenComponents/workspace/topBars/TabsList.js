860750554fe1f58c23b5e8e679598db7943390a6
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 01:19:20 +0530

    add horizontal scrolling

--- system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
@@ -1,36 +1,23 @@
+import { useRef } from "react";
+
 function TabsList() {
+  const ref = useRef(null);
+  const handleScroll = (event) => {
+    const container = ref.current;
+    const scrollAmount = event.deltaY;
+    container.scrollTo({
+      top: 0,
+      left: container.scrollLeft + scrollAmount * 1.3,
+      behavior: "instant",
+    });
+  };
   return (
-    <div className="h-full min-w-[10%] border grow select-none overflow-x-scroll overflow-y-clip hide-scrollbar flex flex-row items-center">
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
-      <span>TabsList</span>
+    <div
+      className="h-full min-w-[10%] border grow select-none overflow-x-scroll overflow-y-clip hide-scrollbar flex flex-row items-center"
+      onWheel={handleScroll}
+      ref={ref}
+    >
+      
     </div>
   );
 }
