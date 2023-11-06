70e57437b2a0811fc4fa59e8f901690739ff3efb
rohan843 <rohan.sharma2934@gmail.com>
2023-11-06 22:56:35 +0530

    deal with clicks the manageable way

--- system-code/src/components/activationAndKeystrokeMonitoring/ActivatableEntity.js
+++ system-code/src/components/activationAndKeystrokeMonitoring/ActivatableEntity.js
@@ -1,30 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setActiveElementTo } from "../../store";
-import { useEffect, useRef } from "react";
 
 // This component wraps around some JSX elements and any time a click is detected
 // on any of those elements, the provided ID is set as the active element's ID.
 // (Repeated clicks on the same ActivatableEntity _donot_ trigger new dispatches;
 // a dispatch is only triggered when the active ID and the provided ID are unequal.)
 function ActivatableEntity({ id, children, ...props }) {
-  const ref = useRef(null);
   const dispatch = useDispatch();
   const { id: activeElementID } = useSelector((store) => store.activeElement);
 
-  useEffect(() => {
-    const handleClick = () => {
-      if (activeElementID !== id) {
-        dispatch(setActiveElementTo(id));
-      }
-    };
-    ref.addEventListener("click", handleClick, { capture: true });
-    return () => {
-      ref.removeEventListener("click", handleClick);
-    };
-  }, [activeElementID, dispatch, id]);
+  const handleClick = () => {
+    if (activeElementID !== id) {
+      dispatch(setActiveElementTo(id));
+    }
+  };
 
   return (
-    <div ref={ref} {...props}>
+    <div onClickCapture={handleClick} {...props}>
       {children}
     </div>
   );
