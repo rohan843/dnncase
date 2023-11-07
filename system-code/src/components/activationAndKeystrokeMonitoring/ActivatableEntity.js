1970405b6c6a6ff305633f5b4bcdfe20a261a7a0
rohan843 <rohan.sharma2934@gmail.com>
2023-11-07 16:45:29 +0530

    remove warning

--- system-code/src/components/activationAndKeystrokeMonitoring/ActivatableEntity.js
+++ system-code/src/components/activationAndKeystrokeMonitoring/ActivatableEntity.js
@@ -7,13 +7,10 @@ import { setActiveElementTo } from "../../store";
 // on any of those elements, the provided ID is set as the active element's ID.
 // (Repeated clicks on the same ActivatableEntity _donot_ trigger new dispatches;
 // a dispatch is only triggered when the active ID and the provided ID are unequal.)
-// 
+//
 // CSS Note: This wraps elements in a div. Any classes (esp. height and width) must be
 // applies to this element just as if they are being applies to a parent div.
 function ActivatableEntity({ id, children, ...props }) {
-
-  console.warn("ActivatableEntity: This component is not complete yet.");
-
   const dispatch = useDispatch();
   const { id: activeElementID } = useSelector((store) => store.activeElement);
 
