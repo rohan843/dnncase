e00cb533fd85815587c71d8a97000597405bb59c
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 01:38:05 +0530

    update example

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ModelCanvas.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ModelCanvas.js
@@ -89,6 +89,7 @@ const rightPaneConfig = [
     innerText:
       "Lorem,\n\nipsum dolor sit amet consectetur adipisicing elit. Atque illum dolorum velit magni eos expedita tempore culpa libero dolore. Sunt fugit porro ducimus aperiam. Modi aliquid sequi odit possimus tempora?",
     onChange: () => {},
+    onEditsInitiated: () => {},
   },
   {
     id: "markdowncodecomment",
