a5e485194a272e5436f7b51d7cc57610ee13ed8f
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 02:44:40 +0530

    possible securing change

--- system-code/native/main.js
+++ system-code/native/main.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow } = require("electron");
 const createWindow = () => {
   const win = new BrowserWindow({ frame: false });
   win.maximize();
+  win.webContents.session.enableNetworkEmulation({ offline: true });
   win.loadURL("http://localhost:3000");
 };
 
