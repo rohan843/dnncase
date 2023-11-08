979028de134f67bd1f7237d556e738c352559613
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 00:08:12 +0530

    create title subcomponent

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/Title.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/Title.js
@@ -1,5 +1,21 @@
-function Title() {
-  return <div>Title</div>;
+import { useRef } from "react";
+import useHorizontalScrolling from "../../../../../../hooks/useHorizontalScroll";
+
+function Title({ config }) {
+  const ref = useRef(null);
+  const scrollHandler = useHorizontalScrolling(ref);
+  if (!config.show) return null;
+  return (
+    <div
+      ref={ref}
+      className="border-darker w-[90%] h-12 background-dark flex items-center text-3xl font-mono mb-2 overflow-y-hidden overflow-x-scroll hide-scrollbar break-keep whitespace-nowrap relative"
+      onWheel={scrollHandler}
+    >
+      <span className="h-full grow"></span>
+      <span className="px-1.5">{config.innerText}</span>
+      <span className="h-full grow"></span>
+    </div>
+  );
 }
 
 export default Title;
