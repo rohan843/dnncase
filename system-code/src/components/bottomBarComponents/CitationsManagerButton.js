0c3969f86e2a7e937ca525aa9cb19aa2ec2209f2
rohan843 <rohan.sharma2934@gmail.com>
2023-11-08 21:10:01 +0530

    update new bottom bar buttons

--- system-code/src/components/bottomBarComponents/CitationsManagerButton.js
+++ system-code/src/components/bottomBarComponents/CitationsManagerButton.js
@@ -1,11 +1,35 @@
 import OptionButton from "./OptionButton";
 import citationsManagerImage from "../../assets/citations.png";
 import ToolTipWrapper from "../tooltipWrapper/ToolTipWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setTerminal,
+  removeTerminal,
+} from "../../store/slices/mainScreenSlice";
+import { TerminalIDs } from "../../constants";
+
+const id = TerminalIDs.citations;
 
 function CitationsManagerButton() {
+  const dispatch = useDispatch();
+
+  const { terminal } = useSelector((store) => {
+    return store.mainScreen;
+  });
+
+  const isActive = terminal === id;
   return (
     <ToolTipWrapper className="h-full" helpText="Citations<br />Manager">
-      <OptionButton active={false} onClick={() => {}}>
+      <OptionButton
+        active={isActive}
+        onClick={() => {
+          if (isActive) {
+            dispatch(removeTerminal());
+          } else {
+            dispatch(setTerminal(id));
+          }
+        }}
+      >
         <img className="h-full" src={citationsManagerImage} alt="" />
       </OptionButton>
     </ToolTipWrapper>
--- system-code/src/components/bottomBarComponents/SystemTerminalButton.js
+++ system-code/src/components/bottomBarComponents/SystemTerminalButton.js
@@ -1,11 +1,35 @@
 import OptionButton from "./OptionButton";
 import terminalImage from "../../assets/terminal.png";
 import ToolTipWrapper from "../tooltipWrapper/ToolTipWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setTerminal,
+  removeTerminal,
+} from "../../store/slices/mainScreenSlice";
+import { TerminalIDs } from "../../constants";
+
+const id = TerminalIDs.systemTerminal;
 
 function SystemTerminalButton() {
+  const dispatch = useDispatch();
+
+  const { terminal } = useSelector((store) => {
+    return store.mainScreen;
+  });
+
+  const isActive = terminal === id;
   return (
     <ToolTipWrapper className="h-full" helpText="System<br />Terminal">
-      <OptionButton active={false} onClick={() => {}}>
+      <OptionButton
+        active={isActive}
+        onClick={() => {
+          if (isActive) {
+            dispatch(removeTerminal());
+          } else {
+            dispatch(setTerminal(id));
+          }
+        }}
+      >
         <img className="h-full" src={terminalImage} alt="" />
       </OptionButton>
     </ToolTipWrapper>
