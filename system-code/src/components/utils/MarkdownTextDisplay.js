1c51850515d7e533a6ab502b37c209a30cf0beb5
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 03:14:48 +0530

    implement secure markdown display

--- system-code/src/components/utils/MarkdownTextDisplay.js
+++ system-code/src/components/utils/MarkdownTextDisplay.js
@@ -8,7 +8,20 @@ import remarkGfm from "remark-gfm";
 function MarkdownTextDisplay({ mdString }) {
   return (
     <span className="this_is_dangerous-unreset-only_for_markdown_use">
-      <Markdown remarkPlugins={[remarkGfm]}>{mdString}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          a(props) {
+            return (
+              <span className={props.className} style={props.style}>
+                {props.children}
+              </span>
+            );
+          },
+        }}
+      >
+        {mdString}
+      </Markdown>
     </span>
   );
 }
