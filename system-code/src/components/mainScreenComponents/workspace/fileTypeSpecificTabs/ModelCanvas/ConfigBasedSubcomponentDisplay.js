fe29719f35c5e24579b04e69dc19d62a7800a9dc
rohan843 <rohan.sharma2934@gmail.com>
2023-11-08 23:29:50 +0530

    create subcomponent display wrapper

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ConfigBasedSubcomponentDisplay.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ConfigBasedSubcomponentDisplay.js
@@ -1,6 +1,16 @@
-import SubcomponentsIndex from "./subcomponents";
+import { SubcomponentsIndex } from "./subcomponents";
 
 function ConfigBasedSubcomponentDisplay({ config }) {
-  return <div>ConfigBasedSubcomponentDisplay</div>;
+  const renderedChildren = config.map((subcomponentConfig) => {
+    const Subcomponent = SubcomponentsIndex[subcomponentConfig.componentType];
+    return (
+      <Subcomponent config={subcomponentConfig} key={subcomponentConfig.id} />
+    );
+  });
+  return (
+    <div className="w-full h-max break-words flex flex-col items-center justify-center">
+      {renderedChildren}
+    </div>
+  );
 }
 export default ConfigBasedSubcomponentDisplay;
