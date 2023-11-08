b0e69e6e1cbc027fe22541c6f02fd52d2dc959d7
rohan843 <rohan.sharma2934@gmail.com>
2023-11-08 19:55:41 +0530

    make panes toggle-able

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/LeftPane.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/LeftPane.js
@@ -1,33 +1,35 @@
 import classNames from "classnames";
 import ToggleButton from "./ToggleButton";
 
-function LeftPane() {
+function LeftPane({ open }) {
   return (
-    <div
-      className={classNames(
-        "w-[400px] h-[95%]",
-        "absolute left-0 top-0 bottom-0 my-auto",
-        "rounded-r-lg",
-        "thin-box-shadow-right",
-        "border-bottom-darker border-top-darker border-right-darker",
-        "background-light",
-        "pr-1 pt-1 pb-1"
-      )}
-    >
+    open && (
       <div
         className={classNames(
-          "w-full h-full",
-          "overflow-x-clip overflow-y-scroll",
-          "p-1",
-          "stable-scrollbar-gutter style-scrollbar-slightly-rounded"
+          "w-[400px] h-[95%]",
+          "absolute left-0 top-0 bottom-0 my-auto",
+          "rounded-r-lg",
+          "thin-box-shadow-right",
+          "border-bottom-darker border-top-darker border-right-darker",
+          "background-light",
+          "pr-1 pt-1 pb-1"
         )}
       >
-        <ToggleButton
-          pointLeft={true}
-          className="absolute -right-2.5 bottom-[5%]"
-        />
+        <div
+          className={classNames(
+            "w-full h-full",
+            "overflow-x-clip overflow-y-scroll",
+            "p-1",
+            "stable-scrollbar-gutter style-scrollbar-slightly-rounded"
+          )}
+        >
+          <ToggleButton
+            pointLeft={true}
+            className="absolute -right-2.5 bottom-[5%]"
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/RightPane.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/RightPane.js
@@ -1,33 +1,35 @@
 import classNames from "classnames";
 import ToggleButton from "./ToggleButton";
 
-function RightPane() {
+function RightPane({ open }) {
   return (
-    <div
-      className={classNames(
-        "w-[400px] h-[95%]",
-        "absolute right-0 top-0 bottom-0 my-auto",
-        "rounded-l-lg",
-        "thin-box-shadow-left",
-        "border-bottom-darker border-top-darker border-left-darker",
-        "background-light",
-        "pl-1 pt-1 pb-1"
-      )}
-    >
+    open && (
       <div
         className={classNames(
-          "w-full h-full",
-          "overflow-x-clip overflow-y-scroll",
-          "p-1",
-          "stable-scrollbar-gutter style-scrollbar-slightly-rounded"
+          "w-[400px] h-[95%]",
+          "absolute right-0 top-0 bottom-0 my-auto",
+          "rounded-l-lg",
+          "thin-box-shadow-left",
+          "border-bottom-darker border-top-darker border-left-darker",
+          "background-light",
+          "pl-1 pt-1 pb-1"
         )}
       >
-        <ToggleButton
-          pointLeft={false}
-          className="absolute -left-2.5 bottom-[5%]"
-        />
+        <div
+          className={classNames(
+            "w-full h-full",
+            "overflow-x-clip overflow-y-scroll",
+            "p-1",
+            "stable-scrollbar-gutter style-scrollbar-slightly-rounded"
+          )}
+        >
+          <ToggleButton
+            pointLeft={false}
+            className="absolute -left-2.5 bottom-[5%]"
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
