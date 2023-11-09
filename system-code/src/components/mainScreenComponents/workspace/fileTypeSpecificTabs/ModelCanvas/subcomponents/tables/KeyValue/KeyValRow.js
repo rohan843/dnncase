10074e40cd37f5447548c69bd01158b53a76afe6
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 19:37:57 +0530

    create remove entry button

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRow.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRow.js
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import removeIcon from "../../../../../../../../assets/remove-key.png";
 
 function KeyValRow({ item, light }) {
   const backgroundColorClass = light
@@ -6,14 +7,25 @@ function KeyValRow({ item, light }) {
     : "background-slightly-dark";
   return (
     <div className="min-h-[2rem] flex flex-row items-center justify-evenly my-1 font-mono">
+      {/* Key */}
       <div
         className={classNames(
-          "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1",
+          "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1 py-2 relative overflow-hidden",
           backgroundColorClass
         )}
       >
+        {item.removable && (
+          <div
+            className="h-2.5 border-black hover-background-dark cursor-pointer active-thin-inset-box-shadow rounded-[1px] w-auto absolute -left-px -top-px"
+            onClick={item.onRemove}
+          >
+            <img src={removeIcon} alt="" className="h-full w-auto" />
+          </div>
+        )}
+
         {item.keyInnerText}
       </div>
+      {/* Value */}
       <div
         className={classNames(
           "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1",
