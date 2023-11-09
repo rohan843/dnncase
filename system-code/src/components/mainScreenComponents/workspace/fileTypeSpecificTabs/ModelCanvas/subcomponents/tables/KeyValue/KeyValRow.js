2c33354dd7ad96b5b1a9e6da6b9029affd3b7f26
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 19:21:25 +0530

    further divide module

---
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRow.js
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+
+function KeyValRow({ item, light }) {
+  const backgroundColorClass = light
+    ? "background-lightest"
+    : "background-slightly-dark";
+  return (
+    <div
+      key={item.keyInnerText}
+      className="min-h-[2rem] flex flex-row items-center justify-evenly my-1 font-mono"
+    >
+      <div
+        className={classNames(
+          "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1",
+          backgroundColorClass
+        )}
+      >
+        {item.keyInnerText}
+      </div>
+      <div
+        className={classNames(
+          "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1",
+          backgroundColorClass
+        )}
+      >
+        {item.valueInnerText}
+      </div>
+    </div>
+  );
+}
+
+export default KeyValRow;
--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRows.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRows.js
@@ -1,33 +1,9 @@
-import classNames from "classnames";
+import KeyValRow from "./KeyValRow";
 
 function KeyValRows({ content }) {
   if (!content || content.length === 0) return null;
   const contentJSX = content.map((item, idx) => {
-    const backgroundColorClass =
-      idx % 2 === 0 ? "background-lightest" : "background-slightly-dark";
-    return (
-      <div
-        key={item.keyInnerText}
-        className="min-h-[2rem] flex flex-row items-center justify-evenly my-1 font-mono"
-      >
-        <div
-          className={classNames(
-            "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1",
-            backgroundColorClass
-          )}
-        >
-          {item.keyInnerText}
-        </div>
-        <div
-          className={classNames(
-            "w-[45%] self-stretch flex items-center justify-evenly break-all border-charcoal rounded px-1",
-            backgroundColorClass
-          )}
-        >
-          {item.valueInnerText}
-        </div>
-      </div>
-    );
+    return <KeyValRow light={idx % 2 === 0} item={item} />;
   });
   return (
     <div className="w-full h-max border-bottom-darker rounded-b overflow-y-scroll hide-scrollbar">
