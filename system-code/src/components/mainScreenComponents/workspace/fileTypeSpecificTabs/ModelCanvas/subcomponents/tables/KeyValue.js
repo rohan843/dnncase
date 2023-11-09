0018ddcfd2876e3bab074d32bff0f211380c0680
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 19:14:03 +0530

    update styles

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue.js
@@ -12,14 +12,14 @@ function TopToolbar({ onAdd, onNewWindow }) {
   return (
     <div className="flex flex-row items-center w-full h-7 border-bottom-darker rounded-b">
       <div
-        className="w-5 h-5 cursor-pointer hover-background-dark rounded flex items-center justify-center hover-border-black mx-1"
+        className="w-5 h-5 cursor-pointer hover-background-dark rounded flex items-center justify-center hover-border-black mx-1 active-thin-inset-box-shadow"
         onClick={onAdd}
       >
         <img src={plusIcon} alt="" className="w-4 h-auto" />
       </div>
       <div className="grow" />
       <div
-        className="w-5 h-5 cursor-pointer hover-background-dark rounded flex items-center justify-center hover-border-black mx-1"
+        className="w-5 h-5 cursor-pointer hover-background-dark rounded flex items-center justify-center hover-border-black mx-1 active-thin-inset-box-shadow"
         onClick={onNewWindow}
       >
         <img src={newWindowIcon} alt="" className="w-4 h-auto" />
