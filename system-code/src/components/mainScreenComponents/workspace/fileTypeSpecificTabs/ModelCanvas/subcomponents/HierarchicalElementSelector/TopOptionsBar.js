7513666d4fb18d81065d85603aaf5a8f3e706e42
rohan843 <rohan.sharma2934@gmail.com>
2023-11-10 01:14:53 +0530

    create top options bar

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/HierarchicalElementSelector/TopOptionsBar.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/HierarchicalElementSelector/TopOptionsBar.js
@@ -1,6 +1,31 @@
+import checkedBoxImage from "../../../../../../../assets/checkbox-checked.png";
+import uncheckedBoxImage from "../../../../../../../assets/checkbox-unchecked.png";
+
 function TopOptionsBar({ options, optionsState, setOptionsState }) {
+  if (options.length === 0) return null;
+  const renderedOptions = options.map((option) => {
+    return (
+      <div className="w-full h-max flex flex-row" key={option.id}>
+        <img
+          src={
+            (optionsState[option.id] && checkedBoxImage) || uncheckedBoxImage
+          }
+          onClick={() => {
+            setOptionsState((state) => {
+              state[option.id] = !state[option.id];
+            });
+          }}
+          alt=""
+          className="mr-1 h-4 mt-[0.25rem] opacity-50"
+        />
+        <label className="break-words hyphens">{option.label}</label>
+      </div>
+    );
+  });
   return (
-    <div className="flex flex-row items-center w-full h-7 border-bottom-darker rounded-b"></div>
+    <div className="flex flex-col items-center w-full min-h-7 max-h-32 px-2 py-1 border-bottom-darker rounded-b overflow-x-hidden overflow-y-scroll hide-scrollbar">
+      {renderedOptions}
+    </div>
   );
 }
 
