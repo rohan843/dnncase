9df9e7d6c6411f2f1eb1157e201219cf17075ee5
rohan843 <rohan.sharma2934@gmail.com>
2023-11-10 01:28:10 +0530

    update styles

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/HierarchicalElementSelector/HierarchicalDisplay.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/HierarchicalElementSelector/HierarchicalDisplay.js
@@ -1,7 +1,7 @@
 function HierarchicalDisplay() {
   return (
-    <div className="w-full h-max max-h-[400px] border-bottom-darker rounded-b overflow-y-scroll hide-scrollbar">
-      jagfsuasgh
+    <div className="w-full h-max max-h-[400px] border-bottom-darker rounded-b overflow-y-scroll hide-scrollbar px-1 py-2">
+      lorem*10
     </div>
   );
 }
