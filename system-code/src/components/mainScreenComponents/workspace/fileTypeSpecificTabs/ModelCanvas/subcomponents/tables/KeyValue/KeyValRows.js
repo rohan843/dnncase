f1b307acaa84b5200cc2bb2b44577c726891797b
rohan843 <rohan.sharma2934@gmail.com>
2023-11-09 20:28:55 +0530

    update code to allow new entry form

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRows.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValRows.js
@@ -1,14 +1,20 @@
 import KeyValRow from "./KeyValRow";
+import NewKeyValInput from "./NewKeyValInput";
 
-function KeyValRows({ content }) {
-  if (!content || content.length === 0) return null;
-  const contentJSX = content.map((item, idx) => {
+function KeyValRows({ config }) {
+  if (!config.content || config.content.length === 0) return null;
+  const contentJSX = config.content.map((item, idx) => {
     return (
       <KeyValRow light={idx % 2 === 0} item={item} key={item.keyInnerText} />
     );
   });
   return (
     <div className="w-full h-max border-bottom-darker rounded-b overflow-y-scroll hide-scrollbar">
+      {config.enableNewKeyValueInput && (
+        <NewKeyValInput
+          onNewKeyValueInputSubmit={config.onNewKeyValueInputSubmit}
+        />
+      )}
       {contentJSX}
     </div>
   );
