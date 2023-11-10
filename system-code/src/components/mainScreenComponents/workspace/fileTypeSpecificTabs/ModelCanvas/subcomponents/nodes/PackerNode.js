ca5fcd636497b4e71c6a471390c55e7719a56fdc
Rohan_Ray123 <rohanray200@gmail.com>
2023-11-10 07:12:16 +0530

    added packer unpacker nodes

---
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/PackerNode.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PackerNode = ()=>{
+    return <div style={{padding:"0px 0px 0px 30px"}}>
+        <div style={{width:"80px",border:"2px solid",height:"150px",
+    borderRadius:"0px 50px 50px 0px",display:"flex",justifyContent:"center"}}>
+            <div style={{margin:"auto"}}>
+                Add image
+            </div>
+        </div>
+    </div>
+}
+
+export default PackerNode;
