fde1bb8871d72e94a66c411d71114b5c70a155ba
rohan843 <rohan.sharma2934@gmail.com>
2023-11-10 22:38:30 +0530

    format code

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/GraphCanvas.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/GraphCanvas.js
@@ -16,12 +16,20 @@ const nodeTypes = {
 };
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } ,type:"selectorNode" },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" },type:"selectorNode" },
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "1" },
+    type: "selectorNode",
+  },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { label: "2" },
+    type: "selectorNode",
+  },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
-
 
 function GraphCanvas() {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
