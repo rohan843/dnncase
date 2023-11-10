a12b1da6c2062349842745586478df1992664417
rohan843 <rohan.sharma2934@gmail.com>
2023-11-10 17:13:56 +0530

    add basic styles

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/CommentNode.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/CommentNode.js
@@ -1,5 +1,9 @@
-function CommentNode() {
+// TODO: Remove margins
 
+function CommentNode() {
+  return (
+    <div className="m-2 w-[353px] h-[185px] border-darker background-dark rounded-t"></div>
+  );
 }
 
 export default CommentNode;
