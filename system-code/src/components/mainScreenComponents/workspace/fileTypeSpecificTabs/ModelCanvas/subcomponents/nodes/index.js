9a089208b97112ec40ec1e6b2d654ac852735c5e
rohan843 <rohan.sharma2934@gmail.com>
2023-11-10 22:37:46 +0530

    update imports and format

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/index.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/index.js
@@ -2,6 +2,14 @@ import ConvLSTM2Dlayer from "./convLSTM2Dlayer";
 import InputNode from "./InputNode";
 import OutputNode from "./OutputNode";
 import UnpackerNode from "./UnpackerNode";
-import PackerNode from "./PackerNode"
+import PackerNode from "./PackerNode";
+import CommentNode from "./CommentNode";
 
-export { ConvLSTM2Dlayer, InputNode, OutputNode, UnpackerNode, PackerNode };
+export {
+  ConvLSTM2Dlayer,
+  InputNode,
+  OutputNode,
+  UnpackerNode,
+  PackerNode,
+  CommentNode,
+};
