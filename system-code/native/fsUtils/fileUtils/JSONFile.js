cb02c35b9f702adffdff7192f73850611639fb16
rohan843 <rohan.sharma2934@gmail.com>
2023-11-12 00:52:50 +0530

    create build and save methods

--- system-code/native/fsUtils/fileUtils/JSONFile.js
+++ system-code/native/fsUtils/fileUtils/JSONFile.js
@@ -7,12 +7,35 @@ class JSONFile extends File {
    * Checks if the provided path points to an existing file. If not, creates that file.
    * @param {(Object | null)} reinitializeIfInvalidSyntax If an object-like, the file contents are parsed and if invalid JSON is found, the whole file is reinitialized to the object provided.
    */
-  syncBuild(reinitializerObjectIfInvalidSyntax = null) {
-    if (!fs.existsSync(this.filePath)) {
-        fs.closeSync(fs.openSync(this.filePath, 'w'));
+  buildSync(reinitializerObjectIfInvalidSyntax = null) {
+    super.buildSync();
+    if (_.isObjectLike(reinitializerObjectIfInvalidSyntax)) {
+      this.__fileData = fs.readFileSync(this.__filePath, "utf8");
+      let parsedResult = null;
+      let wrongSyntax = false;
+      // Validating JSON non-primitive syntax
+      try {
+        parsedResult = JSON.parse(this.__fileData);
+      } catch (e) {
+        wrongSyntax = true;
+      }
+      if (_.isObjectLike(parsedResult)) {
+        wrongSyntax = true;
+      }
+      if (wrongSyntax) {
+        this.__fileData = JSON.stringify(reinitializerObjectIfInvalidSyntax);
+        this.saveSync();
+      }
     }
-    if(_.isObjectLike(reinitializerObjectIfInvalidSyntax)) {
-        const fileData = fs.readFileSync(this.filePath);
+  }
+  /**
+   * Writes the content of the file in-memory to the file in disk.
+   */
+  saveSync() {
+    if (_.isObjectLike(this.__fileData)) {
+      fs.writeFileSync(this.__filePath, JSON.stringify(this.__fileData));
+    } else {
+      fs.writeFileSync(this.__filePath, this.__fileData);
     }
   }
 }
