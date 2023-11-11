eae29ea363644ed8765e1daf01a7eb9c45df530c
rohan843 <rohan.sharma2934@gmail.com>
2023-11-11 23:48:47 +0530

    update comments

--- system-code/native/fsUtils/utils/checkIfChildPathIsValid.js
+++ system-code/native/fsUtils/utils/checkIfChildPathIsValid.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 /**
- * A utility function that takes as input 2 **absolute** paths, `child` and `parent` (both strings) and returns `true` iff the child path is a file or sub-directory within the parent path. Will resolve to `true` if both are same paths.
+ * A utility function that takes as input 2 **absolute** paths, `child` and `parent` (in an object) (both strings) and returns `true` iff the child path is a file or sub-directory within the parent path. Will resolve to `true` if both are same paths.
  */
 function checkIfChildPathIsValid({ child, parent }) {
   child = path.resolve(child);
