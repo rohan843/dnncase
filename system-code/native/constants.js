dc479e5fdd81bc4ee45fa49e98f69360bebcc021
rohan843 <rohan.sharma2934@gmail.com>
2023-11-11 23:27:13 +0530

    include scopes

--- system-code/native/constants.js
+++ system-code/native/constants.js
@@ -1,4 +1,9 @@
 module.exports = {
   appName: "DNNCASE",
   cachedPrevSessionConfigName: "cached-prev-session-config.json",
+  Scope: {
+    global: "global",
+    project: "project",
+    unbounded: "unbounded",
+  },
 };
