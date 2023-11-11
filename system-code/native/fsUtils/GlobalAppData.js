0f1a5ddd7b4ef69375d607a2d5d8dba0a5c93eee
rohan843 <rohan.sharma2934@gmail.com>
2023-11-11 18:03:28 +0530

    create methods and docs

--- system-code/native/fsUtils/GlobalAppData.js
+++ system-code/native/fsUtils/GlobalAppData.js
@@ -1,7 +1,27 @@
+const path = require("node:path");
+const fs = require("fs");
 const { app } = require("electron");
+const { appName } = require("../constants");
 
+/**
+ * A class to allow easy management of global storage area of the system.
+ */
 class GlobalAppData {
-  constructor() {}
+  constructor() {
+    this.appDataDir = path.join(app.getPath("appData"), appName);
+  }
+  /**
+   * Synchronously checks if the global data directory exists.
+   * @returns {boolean} `true` if global directory exists, else `false`;
+   */
+  isDirectoryInitiated() {
+    return fs.existsSync(this.appDataDir);
+  }
+  get getDirPath() {
+    return this.appDataDir;
+  }
 }
 
-module.exports = GlobalAppData;
+const globalAppData = new GlobalAppData();
+
+module.exports = { globalAppData };
