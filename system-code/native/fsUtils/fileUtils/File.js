0ba5525a24a8b0df5939f2e6974fb7376eadeee9
rohan843 <rohan.sharma2934@gmail.com>
2023-11-11 23:17:35 +0530

    begin path validation

--- system-code/native/fsUtils/fileUtils/File.js
+++ system-code/native/fsUtils/fileUtils/File.js
@@ -1,12 +1,16 @@
-// TODO: ensure all paths are valid and lie either in global area or in project area
-
+const RelativePathError = require("../../errors/RelativePathError");
 const path = require("path");
 
 /**
  * The File class. Objects of this class represent a file on the user's system.
  */
 class File {
-  constructor(filePath) {
+  constructor(filePath, fileScope) {
+    if (!path.isAbsolute(filePath)) {
+      throw RelativePathError(
+        `The path: ${filePath} is relative. Scope-based path resolution is currently disabled. Please provide full path.`
+      );
+    }
     this.filePath = filePath;
   }
 }
