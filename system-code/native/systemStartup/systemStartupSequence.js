6e13871996e948a28194b0d6a9e8647dc70ec55d
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 00:57:49 +0530

    imporve comment looks

--- system-code/native/systemStartup/systemStartupSequence.js
+++ system-code/native/systemStartup/systemStartupSequence.js
@@ -1,10 +1,12 @@
 const globalAppData = require("../GlobalAppData");
 
 /**
- * This is the startup sequence. This function will be called once when the 'ready' event is fired. The basic tasks this function does are:
+ * This is the startup sequence. This function will be called once when the 'ready' event is fired. 
+ * The basic tasks this function does are:
  *
  * 1. Check if a last accessed project folder path exists in app data.
- * 2. If it exists, that path is set as the current project's path. Else, `null` is set as the current path.
+ * 2. If it exists, that path is set as the current project's path. Else, `null` is set as the 
+ * current path.
  *
  * @returns {(string | null)} The path to last accessed project folder.
  *
