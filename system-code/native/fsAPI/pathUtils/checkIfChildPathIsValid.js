61d456a66119f1562ed027e50cd4a3fd94a5ece8
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 00:09:39 +0530

    move file

---
+++ system-code/native/fsAPI/pathUtils/checkIfChildPathIsValid.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+/**
+ * A utility function that takes as input 2 **absolute** paths, `child` and `parent` (in an object) 
+ * (both strings) and returns `true` iff the child path is a file or sub-directory within the parent 
+ * path. Will resolve to `true` if both are same paths.
+ */
+function checkIfChildPathIsValid({ child, parent }) {
+  child = path.resolve(child);
+  parent = path.resolve(parent);
+  return child.startsWith(parent);
+}
+
+module.exports = checkIfChildPathIsValid;
