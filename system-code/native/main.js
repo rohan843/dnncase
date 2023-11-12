75001ac1c30d2400815187b58ac969d06752127a
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 01:14:56 +0530

    add missing import

--- system-code/native/main.js
+++ system-code/native/main.js
@@ -4,16 +4,17 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const {
   systemStartupSequence,
 } = require("./systemStartup/systemStartupSequence");
+const path = require("path");
 
 // eslint-disable-next-line no-unused-vars
 const createWindow = () => {
-  const win = new BrowserWindow({ 
+  const win = new BrowserWindow({
     frame: false,
     webPreferences: {
-      preload: path.join(__dirname, './src/backendUtils/preload.js'),
+      preload: path.join(__dirname, "./src/backendUtils/preload.js"),
       nodeIntegration: true,
     },
-   });
+  });
   win.maximize();
   // TODO: Analyse this for its implications on security.
   win.webContents.session.enableNetworkEmulation({ offline: true });
@@ -25,21 +26,18 @@ app.whenReady().then(() => {
   //const currentProjectPath = systemStartupSequence();
   //console.log(currentProjectPath);
 
-  ipcMain.on("minimize",(event,data)=>{
+  ipcMain.on("minimize", (event, data) => {
     console.log(data);
     window.minimize();
   });
 
-  ipcMain.on("maximize",(event,data)=>{
+  ipcMain.on("maximize", (event, data) => {
     console.log(data);
     window.maximize();
   });
 
-  ipcMain.on("close",(event,data)=>{
+  ipcMain.on("close", (event, data) => {
     console.log(data);
     window.close();
   });
 });
-
-
-
