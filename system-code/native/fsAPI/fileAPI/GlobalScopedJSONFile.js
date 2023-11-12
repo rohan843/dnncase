23d63574be916bdcedbd167b2c3c13ac88f9f254
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 00:01:02 +0530

    Add comments

--- system-code/native/fsAPI/fileAPI/GlobalScopedJSONFile.js
+++ system-code/native/fsAPI/fileAPI/GlobalScopedJSONFile.js
@@ -2,6 +2,14 @@ const { globalAppData } = require("../../GlobalAppData");
 const ScopeError = require("../../errors/ScopeError");
 const UnscopedJSONFile = require("./UnscopedJSONFile");
 
+/**
+ * This is the GlobalScopedJSONFile class. Its objects allow access to some file in the global 
+ * project area. This class provides the same methods as the `UnscopedJSONFile`. The only difference 
+ * is that any provided path is validated using the `globalAppData` object's 
+ * `isPathWithinGlobalArea` method to ensure that the path lies in the global area.
+ * 
+ * @throws {ScopeError}
+ */
 class GlobalScopedJSONFile extends UnscopedJSONFile {
   constructor(filePath) {
     if (!globalAppData.isPathWithinGlobalArea(filePath)) {
