002df6640d9dc2888efe55e3c4f43db804101e4c
rohan843 <rohan.sharma2934@gmail.com>
2023-11-12 23:50:32 +0530

    update sub-module exports

--- system-code/native/fsAPI/index.js
+++ system-code/native/fsAPI/index.js
@@ -1,7 +1,15 @@
 const UnscopedFile = require("./fileAPI/UnscopedFile");
 const UnscopedJSONFile = require("./fileAPI/UnscopedJSONFile");
+const GlobalScopedFile = require("./fileAPI/GlobalScopedFile");
+const GlobalScopedJSONFile = require("./fileAPI/GlobalScopedJSONFile");
+const ProjectScopedFile = require("./fileAPI/ProjectScopedFile");
+const ProjectScopedJSONFile = require("./fileAPI/ProjectScopedJSONFile");
 
 module.exports = {
   UnscopedFile,
   UnscopedJSONFile,
+  GlobalScopedFile,
+  GlobalScopedJSONFile,
+  ProjectScopedFile,
+  ProjectScopedJSONFile,
 };
