06a0700c7907a0ca5f5e411650b45342bc6a667d
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 00:17:16 +0530

    remove Scope enum

--- system-code/native/constants.js
+++ system-code/native/constants.js
@@ -1,9 +1,4 @@
 module.exports = {
   appName: "DNNCASE",
   cachedPrevSessionConfigName: "cached-prev-session-config.json",
-  Scope: {
-    global: "global",
-    project: "project",
-    unbounded: "unbounded",
-  },
 };
