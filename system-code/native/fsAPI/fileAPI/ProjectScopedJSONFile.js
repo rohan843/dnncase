76ddc4ffb37a588abf1b4b9b1e8d63b3e4cd1b47
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 00:04:15 +0530

    Add comments

--- system-code/native/fsAPI/fileAPI/ProjectScopedJSONFile.js
+++ system-code/native/fsAPI/fileAPI/ProjectScopedJSONFile.js
@@ -2,6 +2,14 @@ const projectContext = require("../../ProjectContext");
 const ScopeError = require("../../errors/ScopeError");
 const UnscopedJSONFile = require("./UnscopedJSONFile");
 
+/**
+ * This is the ProjectScopedJSONFile class. Its objects allow access to some json file in the 
+ * project area. This class provides the same methods as the `UnscopedFile`. The only difference is 
+ * that any provided path is validated using the `projectContext` object's `isPathWithinProjectArea` 
+ * method to ensure that the path lies in the project area.
+ *
+ * @throws {ScopeError}
+ */
 class ProjectScopedJSONFile extends UnscopedJSONFile {
   constructor(filePath) {
     if (!projectContext.isPathWithinProjectArea(filePath)) {
