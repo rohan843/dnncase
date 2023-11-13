f6d1fac4cf7a6d905c1d46f3e52bfb458c17fd48
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 20:11:44 +0530

    update file type based option display

--- system-code/src/components/mainScreenComponents/workspace/topBars/CurrentlyActiveOptions.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/CurrentlyActiveOptions.js
@@ -1,9 +1,13 @@
 import DCOptions from "./fileTypeSpecificOptions/DCOptions/DCOptions";
+import { useSelector } from "react-redux";
 
-function CurrentlyActiveOptions() {
+function CurrentlyActiveOptions({ activeFileIndex }) {
+  const activeFileType = useSelector(
+    (store) => store.filesystem.fsState[activeFileIndex].data.filetype
+  );
   return (
     <div className="h-full w-max flex items-center px-2">
-      <DCOptions />
+      {activeFileType === "dc" && <DCOptions />}
     </div>
   );
 }
