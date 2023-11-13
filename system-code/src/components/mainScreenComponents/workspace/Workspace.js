e189bf87380b506ffd851bc26574afcad4bff768
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 18:22:37 +0530

    remove state from workspace component

--- system-code/src/components/mainScreenComponents/workspace/Workspace.js
+++ system-code/src/components/mainScreenComponents/workspace/Workspace.js
@@ -3,24 +3,22 @@ import useActivatableEntity from "../../../hooks/useActivatableEntity";
 import TabInnerContent from "./TabInnerContent";
 import QuickAddressBar from "./topBars/QuickAddressBar";
 import TopTabBar from "./topBars/TopTabBar";
-
-const dummyAddressList = [
-  { name: "home1", onClick: () => {} },
-  { name: "user1", onClick: () => {} },
-  { name: "file1.txt", onClick: () => {} },
-];
+import { useSelector } from "react-redux";
 
 function Workspace() {
+  const { openFiles } = useSelector((state) => state.filesystem);
   const handleClickCapture = useActivatableEntity(MajorEntityIDs.workspace);
   return (
-    <div
-      className="w-full h-full z-0 flex flex-col"
-      onClickCapture={handleClickCapture}
-    >
-      <TopTabBar />
-      <QuickAddressBar addressList={dummyAddressList} />
-      <TabInnerContent />
-    </div>
+    openFiles.length > 0 && (
+      <div
+        className="w-full h-full z-0 flex flex-col"
+        onClickCapture={handleClickCapture}
+      >
+        <TopTabBar openFiles={openFiles} />
+        <QuickAddressBar activeFileIndex={openFiles[0].fileIndex} />
+        <TabInnerContent activeFileIndex={openFiles[0].fileIndex} />
+      </div>
+    )
   );
 }
 
