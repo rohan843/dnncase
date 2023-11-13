df75adf5f051666d6fc2530a68045d11ae94ccc5
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 16:33:54 +0530

    update todo

--- system-code/src/backendUtils/preload.js
+++ system-code/src/backendUtils/preload.js
@@ -1,8 +1,13 @@
 const { ipcRenderer, contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-//   send: (channel, data) => ipcRenderer.send(channel, data),
-//   recieve: (channel, func) =>
-//     ipcRenderer.on(channel, (event, ...args) => func(args)),
+  //   send: (channel, data) => ipcRenderer.send(channel, data),
+  //   recieve: (channel, func) =>
+  //     ipcRenderer.on(channel, (event, ...args) => func(args)),
   closeWindow: () => ipcRenderer.send("close-window"),
 });
+
+/**
+ * TODO: ipcRenderer.on('dummy', () => {alert('asdf')})
+ *
+ */
