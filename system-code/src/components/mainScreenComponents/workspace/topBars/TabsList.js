7f2c7d27d3248e183d1db5125e4354a6b08f9d1e
rohan843 <rohan.sharma2934@gmail.com>
2023-11-13 19:20:03 +0530

    partially perform stateless refactor

--- system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/TabsList.js
@@ -1,19 +1,31 @@
 import { useRef } from "react";
 import useHorizontalScrolling from "../../../../hooks/useHorizontalScroll";
 import Tab from "./Tab Components/Tab";
+import { getNameFromFileIndex } from "../../../../utils/getNameFromFileIndex";
 
-function TabsList() {
+function TabsList({ openFiles }) {
   const ref = useRef(null);
   const handleScroll = useHorizontalScrolling(ref);
+  const tabsList = openFiles.map((elt, idx) => {
+    return (
+      <Tab
+        key={elt.fileIndex}
+        name={getNameFromFileIndex(elt.fileIndex)}
+        active={idx === 0}
+        // TODO: Add values for these props
+        unsaved
+        onClose={null}
+        onClickToMakeActive={null}
+      />
+    );
+  });
   return (
     <div
       className="h-full w-max min-w-[10%] grow select-none overflow-x-scroll overflow-y-clip hide-scrollbar flex flex-row items-center"
       onWheel={handleScroll}
       ref={ref}
     >
-      <Tab name="file1.dnnconf" />
-      <Tab name="file2.dnnconf" active />
-      <Tab name="file3.dnnconf" unsaved />
+      {tabsList}
     </div>
   );
 }
