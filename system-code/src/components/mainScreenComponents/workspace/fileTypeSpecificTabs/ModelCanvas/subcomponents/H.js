225201c762a43069fd9af55042a5832c544450a4
rohan843 <rohan.sharma2934@gmail.com>
2023-11-14 23:21:45 +0530

    update props

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/H.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/H.js
@@ -5,10 +5,10 @@ import classNames from "classnames";
 /**
  * This is the 'heading' subcomponent. It can display headings at 3 different levels - 1, 2, and 3.
  */
-function H({ config }) {
+function H({ show, level, innerText }) {
   const ref = useRef(null);
   const scrollHandler = useHorizontalScrolling(ref);
-  if (!config.show) return null;
+  if (!show) return null;
   return (
     <div
       ref={ref}
@@ -16,15 +16,15 @@ function H({ config }) {
       className={classNames(
         "w-[95%] h-10 flex items-center mb-1 overflow-y-hidden overflow-x-scroll hide-scrollbar break-keep whitespace-nowrap select-none",
         {
-          "text-2xl": config.level === 1,
-          "text-xl": config.level === 2,
-          "text-l": config.level === 3,
+          "text-2xl": level === 1,
+          "text-xl": level === 2,
+          "text-l": level === 3,
         }
       )}
     >
-      {config.level === 1 && <span className="grow h-full"></span>}
-      <span>{config.innerText}</span>
-      {config.level === 1 && <span className="grow h-full"></span>}
+      {level === 1 && <span className="grow h-full"></span>}
+      <span>{innerText}</span>
+      {level === 1 && <span className="grow h-full"></span>}
     </div>
   );
 }
