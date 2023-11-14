0a839e9e6e5ea7a14d21663fa54c314427bd04bd
rohan843 <rohan.sharma2934@gmail.com>
2023-11-15 03:14:50 +0530

    add edit button

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/textAreas/MarkdownButtons.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/textAreas/MarkdownButtons.js
@@ -1,26 +1,42 @@
 import classNames from "classnames";
 import mdIcon from "../../../../../../../assets/md.png";
+import editIcon from "../../../../../../../assets/edit-button.png";
 import ToolTipWrapper from "../../../../../../tooltipWrapper/ToolTipWrapper";
 
-function MarkdownButtons({ enabled, className, onClick }) {
+function MarkdownButtons({
+  enabled,
+  editsEnabled,
+  onEditsToggle,
+  className,
+  onClick,
+}) {
   return (
-    <ToolTipWrapper
-      helpText={
-        (enabled && "Convert to<br />plaintext") || "Convert to<br />markdown"
-      }
-      className={classNames(
-        "select-none w-max h-max cursor-pointer",
-        { "opacity-50 hover:opacity-100": !enabled },
-        className
+    <div className={classNames("flex flex-row items-center", className)}>
+      {enabled && (
+        <ToolTipWrapper
+          helpText={(editsEnabled && "View") || "Edit"}
+          className={classNames("select-none w-max h-max cursor-pointer mr-1", {
+            "opacity-50 hover:opacity-100": !editsEnabled,
+          })}
+        >
+          <div className="w-max h-max cursor-pointer" onClick={onEditsToggle}>
+            <img src={editIcon} alt="" className="w-[26px] h-auto" />
+          </div>
+        </ToolTipWrapper>
       )}
-    >
-      <div
-        className="w-max h-max cursor-pointer"
-        onClick={onClick}
+      <ToolTipWrapper
+        helpText={
+          (enabled && "Convert to<br />plaintext") || "Convert to<br />markdown"
+        }
+        className={classNames("select-none w-max h-max cursor-pointer", {
+          "opacity-50 hover:opacity-100": !enabled,
+        })}
       >
-        <img src={mdIcon} alt="" className="w-5 h-auto" />
-      </div>
-    </ToolTipWrapper>
+        <div className="w-max h-max cursor-pointer" onClick={onClick}>
+          <img src={mdIcon} alt="" className="w-5 h-auto" />
+        </div>
+      </ToolTipWrapper>
+    </div>
   );
 }
 
