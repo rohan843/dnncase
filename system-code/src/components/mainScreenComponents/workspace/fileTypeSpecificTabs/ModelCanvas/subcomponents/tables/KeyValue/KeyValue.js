9858264bb4c948a4d0d0381663f707a7fb19f92b
rohan843 <rohan.sharma2934@gmail.com>
2023-11-15 00:34:41 +0530

    update docs

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValue.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/tables/KeyValue/KeyValue.js
@@ -1,6 +1,29 @@
 import TopToolbar from "./TopToolbar";
 import KeyValRows from "./KeyValRows";
 
+/**
+ * A component to display to the user a 2-column table of key-value pairs.
+ * @param {Object} param0
+ * @param {boolean} param0.show If true, the component will return JSX, else it will return `null`.
+ * @param {() => void} param0.onAdd This function will be called when the user clicks the 'add new
+ * pair' btn.
+ * @param {() => void} param0.onNewWindow This function will be called when the user clicks the
+ * 'open in new window' button.
+ * @param {[{
+ * keyInnerText: string,
+ * valueInnerText: string,
+ * isValueEditable: boolean,
+ * onValueChange?: (newValue: string) => void,
+ * removable: boolean,
+ * onRemove?: () => void,
+ * }]} param0.content
+ * @param {boolean} param0.enableNewKeyValueInput If true, a form to take in a new input pair will
+ * be shown.
+ * @param {(inputKey: string, inputValue: string) => void} param0.onNewKeyValueInputSubmit This
+ * function will be called when a user submits a new key-value pair.
+ * @param {() => void} param0.onCancel This function will be called when the user cancels the form
+ * to enter a new key-value pair.
+ */
 function KeyValue({
   show,
   onAdd,
