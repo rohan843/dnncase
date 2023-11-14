84f95c03610d9d848213c5555f316c29051f399f
rohan843 <rohan.sharma2934@gmail.com>
2023-11-15 03:21:18 +0530

    add reducer

--- system-code/src/store/slices/viewConfigsSlice.js
+++ system-code/src/store/slices/viewConfigsSlice.js
@@ -16,7 +16,7 @@ const viewConfigSlice = createSlice({
         comment: {
           text: "# Dummy Text\nLorem, [ipsum]() dolor _sit_ **amet** consectetur adipisicing elit.\n>Atque illum dolorum velit magni eos expedita tempore culpa libero dolore. Sunt fugit porro ducimus aperiam. Modi aliquid sequi odit possimus tempora?",
           type: "plain",
-          markdownEditsEnabled: true
+          markdownEditsEnabled: true,
         },
       },
     },
@@ -37,6 +37,22 @@ const viewConfigSlice = createSlice({
         );
     },
     /**
+     * Takes an action with a payload containing a fileIndex and a list called `editPoints` 
+     * of objects containing a path in its config object (from the config object's root, _not_ the 
+     * slice's root) as per the format given [here](https://lodash.com/docs/4.17.15#set) and a 
+     * value. The value is assigned at the specified path for each object.
+     */
+    setValuesAtPaths(state, action) {
+      if (!state[action.payload.fileIndex]) return;
+      for (let editPoint of action.payload.editPoints) {
+        set(
+          state[action.payload.fileIndex],
+          editPoint.path,
+          editPoint.value
+        );
+      }
+    },
+    /**
      * Writes a fileIndex's config to the state. This will typically be called when initializing the
      * config of some file.
      */
@@ -47,4 +63,4 @@ const viewConfigSlice = createSlice({
 });
 
 export const viewConfigReducer = viewConfigSlice.reducer;
-export const { setConfig, setValueAtPath } = viewConfigSlice.actions;
+export const { setConfig, setValueAtPath, setValuesAtPaths } = viewConfigSlice.actions;
