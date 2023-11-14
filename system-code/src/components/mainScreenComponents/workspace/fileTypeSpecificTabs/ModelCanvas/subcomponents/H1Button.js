afe895ccef37aae8379dcd9618f16b1d26b8f9bd
rohan843 <rohan.sharma2934@gmail.com>
2023-11-14 23:20:54 +0530

    update props

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/H1Button.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/H1Button.js
@@ -1,19 +1,19 @@
 import { useRef } from "react";
 import useHorizontalScrolling from "../../../../../../hooks/useHorizontalScroll";
 
-function H1Button({ config }) {
+function H1Button({ show, onClick, innerText }) {
   const ref = useRef(null);
   const handleScroll = useHorizontalScrolling(ref);
-  if (!config.show) return null;
+  if (!show) return null;
   return (
     <div
       className="w-[95%] h-10 cursor-pointer select-none text-2xl tracking-wide border-darker background-dark hover-background-darker hover-border-black active-thin-inset-box-shadow flex items-center mb-2 flex items-center overflow-y-hidden overflow-x-scroll hide-scrollbar break-keep whitespace-nowrap"
-      onClick={config.onClick}
+      onClick={onClick}
       onWheel={handleScroll}
       ref={ref}
     >
       <span className="h-full grow"></span>
-      <span className="px-1.5">{config.innerText}</span>
+      <span className="px-1.5">{innerText}</span>
       <span className="h-full grow"></span>
     </div>
   );
