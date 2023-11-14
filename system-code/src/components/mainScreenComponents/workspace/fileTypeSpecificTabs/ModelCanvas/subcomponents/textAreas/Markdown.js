41d17f38cb8e8a9d2491484f3eb44306a3867f2d
rohan843 <rohan.sharma2934@gmail.com>
2023-11-15 00:19:50 +0530

    update type and docs

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/textAreas/Markdown.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/textAreas/Markdown.js
@@ -1,9 +1,22 @@
-// TODO: Add config based inputs for all possible props.
+// TODO: Add a method to run an 'onEditsEnable' method.
 
 import CodeEditor from "@uiw/react-textarea-code-editor";
 import MarkdownEnableButton from "./MarkdownEnableButton";
 import MarkdownTextDisplay from "../../../../../../utils/MarkdownTextDisplay";
 
+/**
+ * A component to display a comment styled as a markdown string.
+ * @param {Object} param0
+ * @param {boolean} param0.show If true, the component will return JSX, else it will return `null`.
+ * @param {() => void} param0.onConvertToPlaintext A callback to run when the user converts the
+ * comment to a plaintext comment.
+ * @param {boolean} param0.editsEnabled If true, users will be shown an editable interface. Else,
+ * text styled as markdown will be displayed.
+ * @param {string} param0.innerText The comment string to display.
+ * @param {(newValue: string) => void} param0.onChange This function is fired when the comment is
+ * changed. The new value of comment text is passed as the only argument.
+ * @param {any[]} param0.props Any other props.
+ */
 function Markdown({
   show,
   onConvertToPlaintext,
@@ -18,7 +31,9 @@ function Markdown({
       <MarkdownEnableButton
         enabled={true}
         className="absolute right-0 top-0 z-10"
-        onClick={onConvertToPlaintext}
+        onClick={() => {
+          onConvertToPlaintext && onConvertToPlaintext();
+        }}
       />
       {!editsEnabled && (
         <div className="w-full h-max p-[10px]" {...props}>
@@ -29,7 +44,9 @@ function Markdown({
         /* TODO: CodeEditor doesn't seem to support classes. Either see their support or report this issue.*/
         <CodeEditor
           value={innerText}
-          onChange={onChange}
+          onChange={(e) => {
+            onChange && onChange(e.target.value);
+          }}
           placeholder={null}
           style={{
             backgroundColor: "transparent",
