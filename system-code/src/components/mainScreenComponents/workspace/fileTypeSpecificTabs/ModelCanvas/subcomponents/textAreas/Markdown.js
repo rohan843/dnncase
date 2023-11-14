e57f21950eb8c6d95643a9817f8eb6778bc7ee0b
rohan843 <rohan.sharma2934@gmail.com>
2023-11-14 23:38:59 +0530

    update props

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/textAreas/Markdown.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/textAreas/Markdown.js
@@ -4,32 +4,39 @@ import CodeEditor from "@uiw/react-textarea-code-editor";
 import MarkdownEnableButton from "./MarkdownEnableButton";
 import MarkdownTextDisplay from "../../../../../../utils/MarkdownTextDisplay";
 
-function Markdown({ config, ...props }) {
-  if (!config.show) return null;
+function Markdown({
+  show,
+  onConvertToPlaintext,
+  editsEnabled,
+  innerText,
+  onChange,
+  ...props
+}) {
+  if (!show) return null;
   return (
     <div className="h-max w-[95%] border-darker rounded p-1 mb-2 overflow-hidden whitespace-break-spaces font-mono relative">
       <MarkdownEnableButton
         enabled={true}
         className="absolute right-0 top-0 z-10"
-        onClick={config.onConvertToPlaintext}
+        onClick={onConvertToPlaintext}
       />
-      {!config.editsEnabled && (
+      {!editsEnabled && (
         <div className="w-full h-max p-[10px]" {...props}>
-          <MarkdownTextDisplay mdString={config.innerText} />
+          <MarkdownTextDisplay mdString={innerText} />
         </div>
       )}
-      {config.editsEnabled && (
+      {editsEnabled && (
         /* TODO: CodeEditor doesn't seem to support classes. Either see their support or report this issue.*/
         <CodeEditor
-          value={config.innerText}
-          onChange={config.onChange}
+          value={innerText}
+          onChange={onChange}
           placeholder={null}
           style={{
             backgroundColor: "transparent",
             fontFamily:
               'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
             fontSize: "0.8rem",
-            color: "black"
+            color: "black",
           }}
           {...props}
         />
