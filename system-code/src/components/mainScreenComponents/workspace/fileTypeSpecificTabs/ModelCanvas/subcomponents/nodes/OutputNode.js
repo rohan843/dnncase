8fb008b6d8474d4eebe47da84aa01c642e8b4632
rohan843 <rohan.sharma2934@gmail.com>
2023-11-15 18:21:25 +0530

    add basic handles

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/OutputNode.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/OutputNode.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import { Handle } from "reactflow";
 
-const OutputNode = ()=>{
-    return <div style={{padding:"0px 0px 0px 30px"}}>
-        <div style={{width:"200px",border:"2px solid",height:"100px",
-    borderRadius:"0px 100px 100px 0px"}}>
-            <div style={{padding:"10px"}}>Output Node</div>
-            <div style={{padding:"10px"}}>[4 ,2 ,7]</div>
-        </div>
+const OutputNode = ({ data: { outputShape } }) => {
+  return (
+    <div style={{ padding: "0px 0px 0px 30px" }}>
+      <div
+        style={{
+          width: "200px",
+          border: "2px solid",
+          height: "100px",
+          borderRadius: "0px 100px 100px 0px",
+        }}
+      >
+        <div style={{ padding: "10px" }}>Output Node</div>
+        <div style={{ padding: "10px" }}>{outputShape}</div>
+        <Handle type="target" />
+      </div>
     </div>
-}
+  );
+};
 
 export default OutputNode;
