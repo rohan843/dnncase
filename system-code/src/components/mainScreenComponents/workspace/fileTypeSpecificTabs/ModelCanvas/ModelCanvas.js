a330bc718abb4ec27c87196317c139fa26a0d3b6
rohan843 <rohan.sharma2934@gmail.com>
2023-11-16 03:05:11 +0530

    extract functionality

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ModelCanvas.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/ModelCanvas.js
@@ -22,8 +22,7 @@ import {
   PackerNode,
   CommentNode,
 } from "./subcomponents/nodes";
-import { includes } from "lodash";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 const NodeTypes = {
   LayerNode,
@@ -120,132 +119,6 @@ const initialEdges = [
   },
 ];
 
-function handleParentExpand(res, updateItem) {
-  const parent = res.find((e) => e.id === updateItem.parentNode);
-
-  if (parent) {
-    const extendWidth = updateItem.position.x + updateItem.width - parent.width;
-    const extendHeight =
-      updateItem.position.y + updateItem.height - parent.height;
-
-    if (
-      extendWidth > 0 ||
-      extendHeight > 0 ||
-      updateItem.position.x < 0 ||
-      updateItem.position.y < 0
-    ) {
-      parent.style = { ...parent.style } || {};
-
-      parent.style.width = parent.style.width ?? parent.width;
-      parent.style.height = parent.style.height ?? parent.height;
-
-      if (extendWidth > 0) {
-        parent.style.width += extendWidth;
-      }
-
-      if (extendHeight > 0) {
-        parent.style.height += extendHeight;
-      }
-
-      if (updateItem.position.x < 0) {
-        const xDiff = Math.abs(updateItem.position.x);
-        parent.position.x = parent.position.x - xDiff;
-        parent.style.width += xDiff;
-        updateItem.position.x = 0;
-      }
-
-      if (updateItem.position.y < 0) {
-        const yDiff = Math.abs(updateItem.position.y);
-        parent.position.y = parent.position.y - yDiff;
-        parent.style.height += yDiff;
-        updateItem.position.y = 0;
-      }
-
-      parent.width = parent.style.width;
-      parent.height = parent.style.height;
-    }
-  }
-}
-
-function applyChanges(changes, elements) {
-  // we need this hack to handle the setNodes and setEdges function of the useReactFlow hook for controlled flows
-  if (changes.some((c) => c.type === "reset")) {
-    return changes.filter((c) => c.type === "reset").map((c) => c.item);
-  }
-  const initElements = changes
-    .filter((c) => c.type === "add")
-    .map((c) => c.item);
-
-  return elements.reduce((res, item) => {
-    const currentChanges = changes.filter((c) => c.id === item.id);
-
-    if (currentChanges.length === 0) {
-      res.push(item);
-      return res;
-    }
-
-    const updateItem = { ...item };
-
-    for (const currentChange of currentChanges) {
-      if (currentChange) {
-        switch (currentChange.type) {
-          case "select": {
-            updateItem.selected = currentChange.selected;
-            break;
-          }
-          case "position": {
-            if (typeof currentChange.position !== "undefined") {
-              updateItem.position = currentChange.position;
-            }
-
-            if (typeof currentChange.positionAbsolute !== "undefined") {
-              updateItem.positionAbsolute = currentChange.positionAbsolute;
-            }
-
-            if (typeof currentChange.dragging !== "undefined") {
-              updateItem.dragging = currentChange.dragging;
-            }
-
-            if (updateItem.expandParent) {
-              handleParentExpand(res, updateItem);
-            }
-            break;
-          }
-          case "dimensions": {
-            if (typeof currentChange.dimensions !== "undefined") {
-              updateItem.width = currentChange.dimensions.width;
-              updateItem.height = currentChange.dimensions.height;
-            }
-
-            if (typeof currentChange.updateStyle !== "undefined") {
-              updateItem.style = {
-                ...(updateItem.style || {}),
-                ...currentChange.dimensions,
-              };
-            }
-
-            if (typeof currentChange.resizing === "boolean") {
-              updateItem.resizing = currentChange.resizing;
-            }
-
-            if (updateItem.expandParent) {
-              handleParentExpand(res, updateItem);
-            }
-            break;
-          }
-          case "remove": {
-            return res;
-          }
-          default:
-        }
-      }
-    }
-
-    res.push(updateItem);
-    return res;
-  }, initElements);
-}
-
 function ModelCanvas({ activeFileIndex }) {
   const dispatch = useDispatch();
   const activeFileType = useSelector(
@@ -257,12 +130,7 @@ function ModelCanvas({ activeFileIndex }) {
   }
 
   const [nodes, setNodes] = useState(initialNodes);
-  const onNodesChange = (changes) =>
-    setNodes((items) => applyChanges(changes, items));
-
   const [edges, setEdges] = useState(initialEdges);
-  const onEdgesChange = (changes) =>
-    setEdges((items) => applyChanges(changes, items));
 
   const onEdgeCreation = (newEdgeData) =>
     setEdges([
@@ -286,8 +154,8 @@ function ModelCanvas({ activeFileIndex }) {
         NodeTypes={NodeTypes}
         edges={edges}
         nodes={nodes}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
+        onNodesChange={(newNodes) => setNodes(newNodes)}
+        onEdgesChange={(newEdges) => setEdges(newEdges)}
         onEdgeCreation={onEdgeCreation}
       />
       <LeftPane
