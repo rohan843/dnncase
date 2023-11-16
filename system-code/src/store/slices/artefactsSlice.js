7c77da2c030d483fe446850168eb87f1aa28f22e
rohan843 <rohan.sharma2934@gmail.com>
2023-11-16 19:14:22 +0530

    further design

---
+++ system-code/src/store/slices/artefactsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const artefactsSlice = createSlice({
+  name: "artefacts",
+  initialState: {},
+  reducers: {},
+});
+
+export const artefactsReducer = artefactsSlice.reducer;
