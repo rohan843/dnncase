5583ec8db733ee3f50c8440335e39a020c11cb5f
rohan843 <rohan.sharma2934@gmail.com>
2023-11-18 02:51:17 +0530

    update initial state

--- system-code/src/store/slices/viewConfigsSlice.js
+++ system-code/src/store/slices/viewConfigsSlice.js
@@ -13,6 +13,11 @@ const viewConfigSlice = createSlice({
           show: true,
         },
       },
+      rightPane: {
+        hyperparamKeyValueInput: {
+          enableNewKeyValueInput: false,
+        },
+      },
       graphCanvas: {
         viewport: { x: 0, y: 0, zoom: 1 },
       },
