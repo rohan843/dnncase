fd795fa1392a6da1c2f06ad9671ee6cb5ca083e0
rohan843 <rohan.sharma2934@gmail.com>
2023-11-21 00:20:17 +0530

    add more layers

--- system-code/src/store/slices/artefactsSlice.js
+++ system-code/src/store/slices/artefactsSlice.js
@@ -72,6 +72,136 @@ const artefactsSlice = createSlice({
         defaultInputHandles: ["in"],
         defaultOutputHandles: ["out"],
       },
+      activation: {
+        displayName: "Activation",
+        subcategorization: "Math",
+        defaultHyperparams: [
+          {
+            id: "activation",
+            value: null,
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      activityRegularization: {
+        displayName: "ActivityRegularization",
+        subcategorization: "Regularization",
+        defaultHyperparams: [
+          {
+            id: "l1",
+            value: 0.0,
+          },
+          {
+            id: "l2",
+            value: 0.0,
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      add: {
+        displayName: "Add",
+        subcategorization: "Math",
+        defaultHyperparams: [],
+        defaultInputHandles: ["addend 1", "addend 2"],
+        defaultOutputHandles: ["sum"],
+      },
+      concatenate: {
+        displayName: "Concatenate",
+        subcategorization: "Math",
+        defaultHyperparams: [
+          {
+            id: "axis",
+            value: -1,
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      dropout: {
+        displayName: "Dropout",
+        subcategorization: "Regularization",
+        defaultHyperparams: [
+          {
+            id: "rate",
+            value: null,
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      averagePooling2D: {
+        displayName: "AveragePooling2D",
+        subcategorization: "Pooling",
+        defaultHyperparams: [
+          {
+            id: "pool_size",
+            value: "(2, 2)",
+          },
+          {
+            id: "strides",
+            value: null,
+          },
+          {
+            id: "padding",
+            value: "valid",
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      conv2DTranspose: {
+        displayName: "Conv2DTranspose",
+        subcategorization: "Convolutional",
+        defaultHyperparams: [
+          {
+            id: "filters",
+            value: null,
+          },
+          {
+            id: "kernel_size",
+            value: null,
+          },
+          {
+            id: "strides",
+            value: "(1, 1)",
+          },
+          {
+            id: "padding",
+            value: "valid",
+          },
+          {
+            id: "activation",
+            value: null,
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      dense: {
+        displayName: "Dense",
+        subcategorization: "Dense",
+        defaultHyperparams: [
+          {
+            id: "units",
+            value: null,
+          },
+          {
+            id: "activation",
+            value: null,
+          },
+        ],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
+      flatten: {
+        displayName: "Flatten",
+        subcategorization: "Dense",
+        defaultHyperparams: [],
+        defaultInputHandles: ["inSignal"],
+        defaultOutputHandles: ["outSignal"],
+      },
     },
   },
   reducers: {},
