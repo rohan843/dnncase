1fa37d8a963fba442ab77a31ed5c9127d67195ac
rohan843 <rohan.sharma2934@gmail.com>
2023-11-21 16:51:27 +0530

    create a function to save code to a file

--- system-code/native/saveFile.js
+++ system-code/native/saveFile.js
@@ -1,29 +1,18 @@
 const { app, dialog } = require("electron");
 const fs = require("fs");
+const path = require("path");
 
 function saveFile(content, fileName) {
   // Show save dialog
-  dialog.showSaveDialog(
-    {
-      defaultPath: app.getPath("downloads") + `/${fileName}`,
-      filters: [
-        { name: "All Files", extensions: ["*"] },
-      ],
-    },
-    (filePath) => {
-      // If the user didn't cancel the dialog
+  dialog
+    .showSaveDialog({
+      defaultPath: path.join(app.getPath("downloads"), fileName),
+    })
+    .then(({ filePath }) => {
       if (filePath) {
-        // Write content to the selected file
-        fs.writeFile(filePath, content, (err) => {
-          if (err) {
-            console.error("Error saving file:", err);
-          } else {
-            console.log("File saved successfully!");
-          }
-        });
+        fs.writeFileSync(filePath, content);
       }
-    }
-  );
+    });
 }
 
 module.exports = saveFile;
