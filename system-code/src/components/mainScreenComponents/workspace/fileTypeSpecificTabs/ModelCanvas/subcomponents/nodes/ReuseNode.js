2c9569b2a0b4eba36685a266168a1ff48821b7c2
Rohan_Ray123 <rohanray200@gmail.com>
2023-11-21 19:22:35 +0530

    updating styles

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/ReuseNode.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/ReuseNode.js
@@ -1,16 +1,18 @@
 import LayerNode from "./LayerNode";
-import Packer from "../../../../../../../assets/packer_left.png";
-import unPacker from "../../../../../../../assets/packer_right.png";
+import Packer from "../../../../../../../assets/packer.png";
+import unPacker from "../../../../../../../assets/unpacker.png";
+import { Handle, Position } from "reactflow";
 
 const ReuseNode = ({ obj }) => {
   return (
-    <div className="background-dark flex justify-around w-[400px] h-[200px]">
+    <div className="background-dark border-darker flex justify-around items-center w-[450px] h-[200px]">
+      {inputHandlesList}
       <div className="flex items-center" style={{padding:"5px"}}>
         <img src={Packer} alt="Packer" className="w-[100px] h-[100px]" />
       </div>
       <LayerNode {...obj} />
       <div className="flex items-center" style={{padding:"5px"}}>
-        <img src={unPacker} alt="Packer" className="w-[150px] h-[100px]" />
+        <img src={unPacker} alt="Packer" className="w-[100px] h-[100px]" />
       </div>
     </div>
   );
