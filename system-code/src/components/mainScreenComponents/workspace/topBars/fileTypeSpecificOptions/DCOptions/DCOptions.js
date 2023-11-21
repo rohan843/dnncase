e43a737cda4be7ad0d0dfc52c7583cc758f421f3
rohan843 <rohan.sharma2934@gmail.com>
2023-11-21 17:02:30 +0530

    update btn

--- system-code/src/components/mainScreenComponents/workspace/topBars/fileTypeSpecificOptions/DCOptions/DCOptions.js
+++ system-code/src/components/mainScreenComponents/workspace/topBars/fileTypeSpecificOptions/DCOptions/DCOptions.js
@@ -6,10 +6,12 @@ const permissibleFileTypes = {
 };
 
 function DCOptions({ activeFileType, activeFileIndex }) {
-  const { nodes, edges } = useSelector(
-    (store) => store.filesystem[activeFileIndex].data
+  const fileData = useSelector(
+    (store) => store.filesystem.fsState[activeFileIndex]
   );
   if (!permissibleFileTypes[activeFileType]) return null;
+  const nodes = fileData.data.nodes;
+  const edges = fileData.data.edges;
   return (
     <div className="h-full w-max flex items-center">
       {/* TODO: Generate python code on click here */}
