04f35554630c1e52ec9a678b9b1646f231d5740c
GKS02 <gurkaran.ug20@nsut.ac.in>
2023-11-22 17:15:35 +0530

    almost_final_code

--- system-code/native/CodeExporterAPI/code_generator.js
+++ system-code/native/CodeExporterAPI/code_generator.js
@@ -1,139 +1,203 @@
-const jsonObject = require("./grahJson");
+const jsonObject = require("./reuse_test");
 const fs = require("fs");
 const artifact = require("./artifacts_test");
-fs.open("code_generated.txt", "w", (err, file) => {
-  if (err) throw err;
-  console.log(file);
-});
+
+var str = "";
 // Importing layer used
-layer_list = [];
-for (const layer of jsonObject.nodes) {
-  if (!layer_list.includes(layer.data.artefactID)) {
-    var imp = "from keras.layer import " + layer.data.artefactID + "\n";
-    fs.appendFileSync("code_generated.txt",imp, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      }
-    });
-    console.log(imp);
-    layer_list.push(layer.data.artefactID);
-  }
-}
-
-// old code
-
+const layer_list = [];
 const inpt_layers = [];
 
-const node = new Map();
-const node_inp = new Map();
-const node_out = new Map();
-const nde_inpt_list = new Map();
-const nde_out_list = new Map();
-for (const layer of jsonObject.nodes) {
-  if (layer.type == "InputNode") {
-    inpt_layers.push(layer.id);
-  }
-  node.set(layer.id, [
-    layer.type,
-    layer.data.artefactID,
-    layer.data.hyperparams,
-    layer.data.commentText,
-    layer.data.reusable,
-    layer.data.reuseCount,
-  ]);
-  node_inp.set(layer.id, layer.data.inputHandles.length);
-  node_out.set(layer.id, layer.data.outputHandles.length);
-  nde_inpt_list.set(layer.id, new Array(layer.data.inputHandles.length));
-  nde_out_list.set(layer.id, []);
-}
-console.log(inpt_layers);
-console.log(nde_inpt_list);
+const node = new Map(); // layerid Vs imp infor
+const node_inp = new Map(); // layerid Vs number of inputs
+const node_out = new Map(); // layerid vs outputs
+const nde_inpt_list = new Map(); // layerdid vs inputList
+const nde_out_list = new Map(); // output list
+const resuse_node = new Map();
 
+const out_list = [];
 // adjacency list
 const adjList = new Map();
-
-for (const layer of jsonObject.edges) {
-  const submap = new Map(); // for storing sourceHandles
-  const target_list = [];
-  target_list.push(layer.target);
-  target_list.push(layer.targetHandle);
-  //console.log(target_list)
-  //console.log(layer.source)
-  if (adjList.has(layer.source)) {
-    const sourceMap = adjList.get(layer.source);
-    if (sourceMap.has(layer.sourceHandle)) {
-      sourceMap.get(layer.sourceHandle).push(target_list);
-    } else {
-      sourceMap.set(layer.sourceHandle, [target_list]);
-    }
-  } else {
-    submap.set(layer.sourceHandle, []);
-    submap.get(layer.sourceHandle).push(target_list);
-    adjList.set(layer.source, submap);
-  }
-  //console.log(adjList);
-}
-// console.log("start")
-console.log(adjList);
-
 let global_cnt = 1;
-const ipList =[]
-for (let i = 0; i < inpt_layers.length; i++) {
-  var temp = temp_var();
-  ipList.push(temp);
-  //console.log(node.get(inpt_layers[i]));
-  var code_gen = temp + "=" + node.get(inpt_layers[i])[1] + "()\n";
-  fs.appendFileSync("code_generated.txt",code_gen, (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-    }
-  });
-  console.log(code_gen);
-  //fs.write("code_generated.txt",code_gen);
-  dfs(
-    adjList.get(inpt_layers[i]).get("0")[0][0], // target id
-    adjList.get(inpt_layers[i]).get("0")[0][1], // target handle
-    temp
-  );
-  //console.log(adjList)
-}
-
-function dfs(node_id, node_idx, curr_inp) {
-  //console.log(node_id)
-  if (node.get(node_id)[0] == "OutputNode") {
-    code_gen = "Model(" + "inputs="+ ipList +", outputs=" + curr_inp + ")\n";
-    fs.appendFileSync("code_generated.txt",code_gen, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
+let reuse_cnt=1;
+function getCodeFrom() {
+  for (const layer of jsonObject.nodes) {
+    if (!layer_list.includes(layer.layerName)) {
+      var imp = "";
+      if (layer.type == "LayerNode" || layer.type == "ReuseNode") {
+        imp = "from keras.layer import " + layer.layerName + "\n";
+        str = str.concat(imp);
+        layer_list.push(layer.layerName);
       }
-    });
-    console.log(code_gen);
-    return;
+      if (layer.type == "InputNode") {
+        imp = "from keras.layer import Input" + "\n";
+        str = str.concat(imp);
+        layer_list.push(layer.layerName);
+      }
+    }
   }
-  /**
-  if (node.get(node_id) == "reuse") {
-    nde_inpt_list.get(node_id).shift()
-    nde_inpt_list.get(node_id).splice(node_idx,0, curr_inp); // ** may give error **
-    var cnt = node_inp.get(node_id) - 1;
-    node_inp.set(node_id, cnt);
-    //console.log(nde_inpt_list.get(node_id));
-    var temp = temp_var();
-    //nde_out_list.get(node_id).push(temp)
-    adjList.get(node_id).get(node_idx).push(temp);
-    var code_gen =
-      temp + "=" + node.get(node_id) + "(" + nde_inpt_list.get(node_id) + ")";
-    console.log(code_gen);
 
+  for (const layer of jsonObject.nodes) {
+    if (layer.type == "InputNode") {
+      inpt_layers.push(layer.id);
+      node.set(layer.id, [layer.type, layer.hyperparams, layer.commentText]);
+      node_inp.set(layer.id, 0);
+      node_out.set(layer.id, 1);
+      nde_inpt_list.set(layer.id, new Array(0));
+      nde_out_list.set(layer.id, []);
+    } else if (layer.type == "LayerNode") {
+      node.set(layer.id, [
+        layer.type,
+        layer.layerName,
+        layer.hyperparams,
+        layer.commentText,
+      ]);
+      node_inp.set(layer.id, layer.inputHandles.length);
+      node_out.set(layer.id, layer.outputHandles.length);
+      nde_inpt_list.set(layer.id, new Array(layer.inputHandles.length));
+      nde_out_list.set(layer.id, []);
+    } else if (layer.type == "ReuseNode") {
+      node.set(layer.id, [
+        layer.type,
+        layer.layerName,
+        layer.hyperparams,
+        layer.commentText,
+      ]);
+      node_inp.set(layer.id, layer.inputHandles.length);
+      node_out.set(layer.id, layer.outputHandles.length);
+      nde_inpt_list.set(layer.id, new Array(layer.inputHandles.length));
+      nde_out_list.set(layer.id, []);
+      
+    } else if (layer.type == "PackerNode") {
+      node.set(layer.id, [layer.type]);
+      node_inp.set(layer.id, layer.inputHandles.length);
+      node_out.set(layer.id, 1);
+      nde_inpt_list.set(layer.id, new Array(layer.inputHandles.length));
+      nde_out_list.set(layer.id, []);
+    } else if (layer.type == "UnpackerNode") {
+      node.set(layer.id, [layer.type]);
+      node_inp.set(layer.id, 1);
+      node_out.set(layer.id, layer.outputHandles.length);
+      nde_inpt_list.set(layer.id, new Array(1));
+      nde_out_list.set(layer.id, []);
+    } else if (layer.type == "OutputNode") {
+      node.set(layer.id, [layer.type, layer.commentText]);
+      node_inp.set(layer.id, 1);
+      node_out.set(layer.id, 0);
+      nde_inpt_list.set(layer.id, new Array(1));
+      nde_out_list.set(layer.id, []);
+    }
+  }
+  console.log(inpt_layers);
+  console.log(nde_inpt_list);
+
+  for (const layer of jsonObject.edges) {
+    const submap = new Map(); // for storing sourceHandles
+    const target_list = [];
+    target_list.push(layer.target);
+    target_list.push(layer.targetHandle);
+    //console.log(target_list)
+    //console.log(layer.source)
+    if (adjList.has(layer.source)) {
+      const sourceMap = adjList.get(layer.source);
+      if (sourceMap.has(layer.sourceHandle)) {
+        sourceMap.get(layer.sourceHandle).push(target_list);
+      } else {
+        sourceMap.set(layer.sourceHandle, [target_list]);
+      }
+    } else {
+      submap.set(layer.sourceHandle, []);
+      submap.get(layer.sourceHandle).push(target_list);
+      adjList.set(layer.source, submap);
+    }
+    //console.log(adjList);
+  }
+  // console.log("start")
+  console.log(adjList);
+
+  const ipList = [];
+
+  for (let i = 0; i < inpt_layers.length; i++) {
+    var temp = temp_var();
+    ipList.push(temp);
+    //console.log(node.get(inpt_layers[i]));
+    const hyperparam = printHyperparams(inpt_layers[i]);
+    var code_gen = temp + "= Input" + "(" + hyperparam + ")\n";
+    str = str.concat(code_gen);
+    console.log(code_gen);
+    //fs.write("code_generated.txt",code_gen);
     dfs(
-      adjList.get(node_id).get(node_idx)[0],
-      adjList.get(node_id).get(node_idx)[1],
-      adjList.get(node_id).get(node_idx)[2]
+      adjList.get(inpt_layers[i]).get("0")[0][0], // target id
+      adjList.get(inpt_layers[i]).get("0")[0][1], // target handle
+      temp
     );
+    //console.log(adjList)
+  }
+  var op =
+    "model_output(inputs= (" + ipList + ") , outputs= (" + out_list + "))\n";
+  str = str.concat(op);
+  return str;
+}
+console.log(getCodeFrom());
+function dfs(node_id, node_idx, curr_inp) {
+  //console.log("hello" + node_id);
+  //console.log(node.get(node_id))
+  if (node.get(node_id)[0] == "OutputNode") {
+    //TODO: cretae output list
+    out_list.push(curr_inp);
     return;
   }
-  */
+  
+  if (node.get(node_id)[0] == "ReuseNode") {
+      if(!resuse_node.has(node_id)){
+        var reuse_temp = temp_var_reuse();
+        resuse_node.set(node_id,reuse_temp);
+        var hyperparam = printHyperparams(node_id)
+        var code_gen = reuse_temp + "=" + node.get(node_id)[1] + "(" + hyperparam + ")\n";
+        //console.log(code_gen);
+        str =str.concat(code_gen);
+      }
+      // unpacker
+      const unpacker_list=[];
+      for(let i=1;i<=node_inp.get(node_id);i++){
+        unpacker_list.push(temp_var());
+      }
+      var temp_str= unpacker_list + "=" + "(" + curr_inp + ")\n";
+      //console.log(temp_str);
+      str = str.concat(temp_str);
+
+      // layer ouputs or packer inputs
+      const packer_list=[];
+      for(let i=1;i<=node_out.get(node_id);i++){
+        packer_list.push(temp_var());
+      }
+      var temp_str1 = packer_list + "=" + resuse_node.get(node_id) + "(" + unpacker_list + ")\n";
+      //console.log(temp_str1);
+      str=str.concat(temp_str1);
+   
+      // packer output
+      var temp = temp_var();
+
+      var temp_str2 = temp + "=" + "("+ packer_list +")\n";
+      //console.log(temp_str2);
+      str = str.concat(temp_str2);
+      
+      adjList.get(node_id).get(node_idx)[0].push(temp);
+
+      const target_arr = adjList.get(node_id).get(node_idx);
+      //console.log(target_arr)
+      //console.log(target_arr[0]);
+
+      //console.log("Intermediate String :"  + str);
+      dfs(target_arr[0][0],target_arr[0][1],target_arr[0][2]);
+
+      return;
+        
+
+  }
+  
+
   //adding tne input to respective node
-  nde_inpt_list.get(node_id).splice(node_idx, 1, curr_inp); // ** may give error **
+  nde_inpt_list.get(node_id).splice(node_idx, 1, curr_inp);
   var cnt = node_inp.get(node_id) - 1;
   node_inp.set(node_id, cnt);
 
@@ -149,30 +213,41 @@ function dfs(node_id, node_idx, curr_inp) {
         targetArray[i].push(temp);
       }
     }
-    var comment = "/**" + node.get(node_id)[3] + "*/\n"
-    fs.appendFileSync("code_generated.txt",comment, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      }
-    });
-    console.log(comment);
-    var code_gen =
-      nde_out_list.get(node_id) +
-      "=" +
-      node.get(node_id)[1] +
-      "(" +
-      node.get(node_id)[2]+
-      ")" +
-      "(" +
-      nde_inpt_list.get(node_id) +
-      ")\n";
-      fs.appendFileSync("code_generated.txt",code_gen, (err) => {
-        if (err) {
-          console.error('Error writing to file:', err);
-        }
-      });
-    console.log(code_gen);
 
+    if (node.get(node_id)[0] == "LayerNode") {
+      var comment = "```" + node.get(node_id)[3] + "```\n";
+      str = str.concat(comment);
+      var hyperparam = printHyperparams(node_id);
+      
+      var code_gen =
+        nde_out_list.get(node_id) +
+        "=" +
+        node.get(node_id)[1] +
+        "(" +
+        hyperparam +
+        ")" +
+        "(" +
+        nde_inpt_list.get(node_id) +
+        ")\n";
+      str = str.concat(code_gen);
+    } else if (node.get(node_id)[0] == "PackerNode") {
+      var code_gen =
+        nde_out_list.get(node_id) +
+        "=" +
+        "(" +
+        nde_inpt_list.get(node_id) +
+        ")\n";
+      str = str.concat(code_gen);
+    } else if (node.get(node_id)[0] == "UnpackerNode") {
+      nde_out_list.get(node_id) +
+        "=" +
+        "(" +
+        nde_inpt_list.get(node_id) +
+        ")\n";
+      str = str.concat(code_gen);
+    }
+
+    //console.log(comment);
     var output_idx = adjList.get(node_id);
     for (const [key, value] of output_idx) {
       for (let i = 0; i < value.length; i++) {
@@ -189,4 +264,46 @@ function temp_var() {
   global_cnt++;
 
   return str;
+}
+
+function temp_var_reuse() {
+  var str = "l" + reuse_cnt;
+  reuse_cnt++;
+
+  return str;
+}
+
+function printHyperparams(node_id) {
+  let hyperparams = [];
+  if (node.get(node_id)[0] == "InputNode") {
+    hyperparams = node.get(node_id)[1];
+  } else if (
+    node.get(node_id)[0] == "LayerNode" ||
+    node.get(node_id)[0] == "ReuseNode"
+  ) {
+    hyperparams = node.get(node_id)[2];
+  }
+  //console.log(hyperparams);
+  var hypram = "";
+  hyperparams.forEach((items) => {
+    const id = items.id;
+    let value = items.value;
+
+    if (value[0] >= "0" && value[0] <= "9") {
+      value = parseInt(value);
+    } else if (
+      (value[0] >= "a" && value[0] <= "z") ||
+      (value[0] >= "A" && value[0] <= "Z")
+    ) {
+      value = "'" + value + "'";
+    }
+
+    var abc = id + "=" + value + ",";
+    hypram= hypram.concat(abc);
+   
+  });
+
+  hypram = hypram.slice(0, -1);
+  
+  return hypram;
 }
