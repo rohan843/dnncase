71646b20f3a39ed03fd1a11261f2f253cda52578
rohan843 <rohan.sharma2934@gmail.com>
2024-04-30 18:04:17 +0530

    chore: remove debug info

--- system-code/src/App.js
+++ system-code/src/App.js
@@ -1,28 +1,19 @@
 import TopBar from "./components/topBarComponents/TopBar";
 import MainScreen from "./components/mainScreenComponents/MainScreen";
 import BottomBar from "./components/bottomBarComponents/BottomBar";
-import DataVariableIn from "./components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/DataVariable.js";
-import DataVariableOut from "./components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/DataVariableOutput.js";
 import { MajorEntityIDs } from "./constants";
 import useActivatableEntity from "./hooks/useActivatableEntity";
-import Input from "./components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/Input.js";
-
 
 function App() {
   const handleClickCapture = useActivatableEntity(MajorEntityIDs.base);
-  // return(
-  //   <DataVariableOut></DataVariableOut>
-  // )
   return (
     <div
       className="h-full w-screen background-light"
       onClickCapture={handleClickCapture}
     >
-      {/* <TopBar />
+      <TopBar />
       <MainScreen />
-      <BottomBar /> */}
-      <DataVariableOut content="hp_units"></DataVariableOut>
-
+      <BottomBar />
     </div>
   );
 }
