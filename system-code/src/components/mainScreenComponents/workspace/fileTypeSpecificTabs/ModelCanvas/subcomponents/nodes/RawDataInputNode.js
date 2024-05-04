ac481ced1d75e5168a2c2afe848a20af49500f14
rohan843 <rohan.sharma2934@gmail.com>
2024-05-04 18:36:15 +0530

    fix: add raw data node ui

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/RawDataInputNode.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/RawDataInputNode.js
@@ -6,31 +6,42 @@ const RawDataInputNode = ({
   selected,
 }) => {
   return (
-    <div
-      onClick={() => {
-        onActivate && onActivate();
-      }}
-      onDoubleClick={() => {
-        onActivateAndShowInPane && onActivateAndShowInPane();
-      }}
-    >
+    <div className="grid grid-cols-1 grid-rows-1 w-[180px] h-[120px]">
       <div
         style={{
-          width: "165px",
-          height: "50px",
-          borderRadius: "0px 100px 100px 0px",
+          width: "180px",
+          height: "120px",
+          clipPath:
+            "polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%)",
         }}
         className={classNames(
-          "bg-[#F0A30A] border flex flex-col justify-center pl-1",
+          "scale-[1.03]",
           {
-            "border-black": selected,
-            "border-[#BD7000]": !selected,
+            "bg-[#000]": selected,
+            "bg-[#6D1F00]": !selected
           }
         )}
+      />
+      <div
+        style={{
+          width: "180px",
+          height: "120px",
+          clipPath:
+            "polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%)",
+        }}
+        className={classNames(
+          "bg-[#A0522D] text-white flex flex-col justify-center items-center"
+        )}
+        onClick={() => {
+          onActivate && onActivate();
+        }}
+        onDoubleClick={() => {
+          onActivateAndShowInPane && onActivateAndShowInPane();
+        }}
       >
-        <div className="text-xl">Raw Data Source</div>
-        <Handle type="source" id="out" position={Position.Right} />
+        <p className="text-xl">Raw Data Source</p>
       </div>
+      <Handle type="source" id="out" position={Position.Right} />
     </div>
   );
 };
