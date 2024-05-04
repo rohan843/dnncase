7cfe1d69ff53e0b25f5db65c34e839fb378b059c
rohan843 <rohan.sharma2934@gmail.com>
2024-05-04 18:17:26 +0530

    fix: add input node styles

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/Input.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/Input.js
@@ -1,12 +1,28 @@
-const InputNode = (
-  {
-    data: { hyperparams, onActivate, onActivateAndShowInPane },
-    selected,
-  }
-) => {
+import classNames from "classnames";
+import { Handle, Position } from "reactflow";
+
+const InputNode = ({
+  data: { hyperparams, onActivate, onActivateAndShowInPane },
+  selected,
+}) => {
   return (
-    <div className="InputNodeStyles">
-      <p>layer_count</p>
+    <div className="w-[153px] h-[100px] grid grid-cols-1 grid-rows-1 justify-center">
+      <div
+        className={classNames(
+          "InputNodeStyles scale-105 relative left-[1.5px]",
+          {
+            "!bg-[#000000]": selected,
+            "!bg-[#001DBC]": !selected,
+          }
+        )}
+      ></div>
+      <div
+        className="InputNodeStyles flex flex-col items-start justify-center"
+        title="layer_count"
+      >
+        <p className="truncate pl-2 w-[60%]">layer_count</p>
+      </div>
+      <Handle id="out" type="source" position={Position.Right} />
     </div>
   );
 };
