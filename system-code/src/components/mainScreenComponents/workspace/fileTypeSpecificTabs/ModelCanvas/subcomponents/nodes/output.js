5b28039cac5fcfd6c7342f0feadd95d3cd763bae
rohan843 <rohan.sharma2934@gmail.com>
2024-05-04 18:24:33 +0530

    fix: add output node

--- system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/output.js
+++ system-code/src/components/mainScreenComponents/workspace/fileTypeSpecificTabs/ModelCanvas/subcomponents/nodes/output.js
@@ -1,7 +1,28 @@
-const OutputNode = () => {
+import classNames from "classnames";
+import { Handle, Position } from "reactflow";
+
+const OutputNode = ({
+  data: { hyperparams, onActivate, onActivateAndShowInPane },
+  selected,
+}) => {
   return (
-    <div className="OutputNodeStyles">
-      <p>layer_count</p>
+    <div className="w-[153px] h-[100px] grid grid-cols-1 grid-rows-1 justify-center">
+      <div
+        className={classNames(
+          "OutputNodeStyles scale-105 relative",
+          {
+            "!bg-[#000000]": selected,
+            "!bg-[#001DBC]": !selected,
+          }
+        )}
+      ></div>
+      <div
+        className="OutputNodeStyles flex flex-col items-end justify-center relative left-[1.5px]"
+        title="layer_count"
+      >
+        <p className="truncate pr-2 w-[60%]">layer_count</p>
+      </div>
+      <Handle id="in" type="target" position={Position.Left} />
     </div>
   );
 };
