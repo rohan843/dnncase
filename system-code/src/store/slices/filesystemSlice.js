185799edf773ec30f20c1fc2f643dc0041154076
Rohan_Ray123 <rohanray200@gmail.com>
2024-05-05 19:22:53 +0530

    Added few artifacts for keras tuner

--- system-code/src/store/slices/filesystemSlice.js
+++ system-code/src/store/slices/filesystemSlice.js
@@ -255,19 +255,104 @@ const filesystemSlice = createSlice({
         children: [],
       },
       "/Demo Project/tuner/tmp1.dc": {
-        index: "/Demo Project/tuner/tmp1.dc",
-        data: {
-          name: "tmp1.dc",
-          folder: false,
-          artefact: false,
-          unsaved: true,
-          filetype: "dc",
-          artefacttype: "ArtefactType",
-          nodes: [],
-          edges: [],
+        "index": "/Demo Project/tuner/tmp1.dc",
+        "data": {
+          "name": "tmp1.dc",
+          "folder": false,
+          "artefact": false,
+          "unsaved": true,
+          "filetype": "dc",
+          "artefacttype": "ArtefactType",
+          "nodes": [
+            {
+              "id": "FunctionNode_CreateAndApplyDenseLayer_5888",
+              "position": { "x": 330, "y": 85 },
+              "type": "FunctionNode",
+              "data": {
+                "name": "Create and Apply Dense Layer",
+                "hyperparams": [
+                  { "id": "units", "value": null },
+                  { "id": "activation", "value": "None" }
+                ],
+                "commentText": "",
+                "commentType": "plain",
+                "inputHandles": ["payload"],
+                "outputHandles": ["payload"],
+                "elementID": "CreateAndApplyDenseLayer"
+              },
+              "width": 353,
+              "height": 203,
+              "selected": false,
+              "positionAbsolute": { "x": 330, "y": 85 },
+              "dragging": false
+            },
+            {
+              "id": "Input_7356",
+              "position": { "x": 5, "y": 235 },
+              "type": "Input",
+              "data": { "name": "hp_units" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 5, "y": 235 },
+              "dragging": false
+            },
+            {
+              "id": "Input_2507",
+              "position": { "x": 10, "y": 65 },
+              "type": "Input",
+              "data": { "name": "payload" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 10, "y": 65 },
+              "dragging": false
+            },
+            {
+              "id": "Output_5171",
+              "position": { "x": 795, "y": 135 },
+              "type": "Output",
+              "data": { "name": "payload" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 795, "y": 135 },
+              "dragging": false
+            }
+          ],
+          "edges": [
+            {
+              "id": "Output_5171_in",
+              "source": "FunctionNode_CreateAndApplyDenseLayer_5888",
+              "sourceHandle": "payload",
+              "target": "Output_5171",
+              "targetHandle": "in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "FunctionNode_CreateAndApplyDenseLayer_5888_payload",
+              "source": "Input_2507",
+              "sourceHandle": "out",
+              "target": "FunctionNode_CreateAndApplyDenseLayer_5888",
+              "targetHandle": "payload",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "FunctionNode_CreateAndApplyDenseLayer_5888_multi-in+6447",
+              "source": "Input_7356",
+              "sourceHandle": "out",
+              "target": "FunctionNode_CreateAndApplyDenseLayer_5888",
+              "targetHandle": "multi-in",
+              "label": "units"
+            }
+          ]
         },
-        isFolder: false,
-        children: [],
+        "isFolder": false,
+        "children": []
       },
       "/Demo Project/tuner/CompiledClassifierModel.dc": {
         index: "/Demo Project/tuner/CompiledClassifierModel.dc",
@@ -285,34 +370,382 @@ const filesystemSlice = createSlice({
         children: [],
       },
       "/Demo Project/tuner/MNISTDataLoader.dc": {
-        index: "/Demo Project/tuner/MNISTDataLoader.dc",
-        data: {
-          name: "MNISTDataLoader.dc",
-          folder: false,
-          artefact: false,
-          unsaved: true,
-          filetype: "dc",
-          artefacttype: "DataLoaderType",
-          nodes: [],
-          edges: [],
+        "index": "/Demo Project/tuner/MNISTDataLoader.dc",
+        "data": {
+          "name": "MNISTDataLoader.dc",
+          "folder": false,
+          "artefact": false,
+          "unsaved": true,
+          "filetype": "dc",
+          "artefacttype": "DataLoaderType",
+          "nodes": [
+            {
+              "id": "FunctionNode_ScalarDivide_7702",
+              "position": { "x": 660, "y": 80 },
+              "type": "FunctionNode",
+              "data": {
+                "name": "Divide by Scalar",
+                "hyperparams": [],
+                "commentText": "",
+                "commentType": "plain",
+                "inputHandles": ["dividend", "divisor"],
+                "outputHandles": ["result"],
+                "elementID": "ScalarDivide"
+              },
+              "width": 353,
+              "height": 203,
+              "selected": false,
+              "positionAbsolute": { "x": 660, "y": 80 },
+              "dragging": false
+            },
+            {
+              "id": "FunctionNode_ScalarDivide_2003",
+              "position": { "x": 660, "y": -155 },
+              "type": "FunctionNode",
+              "data": {
+                "name": "Divide by Scalar",
+                "hyperparams": [],
+                "commentText": "",
+                "commentType": "plain",
+                "inputHandles": ["dividend", "divisor"],
+                "outputHandles": ["result"],
+                "elementID": "ScalarDivide"
+              },
+              "width": 353,
+              "height": 203,
+              "selected": true,
+              "positionAbsolute": { "x": 660, "y": -155 },
+              "dragging": false
+            },
+            {
+              "id": "FunctionNode_LoadMNISTDataset_6724",
+              "position": { "x": 80, "y": 160 },
+              "type": "FunctionNode",
+              "data": {
+                "name": "Load MNIST Dataset",
+                "hyperparams": [],
+                "commentText": "",
+                "commentType": "plain",
+                "inputHandles": [],
+                "outputHandles": ["x_train", "y_train", "x_test", "y_test"],
+                "elementID": "LoadMNISTDataset"
+              },
+              "width": 353,
+              "height": 203,
+              "selected": false,
+              "positionAbsolute": { "x": 80, "y": 160 },
+              "dragging": false
+            },
+            {
+              "id": "Output_2250",
+              "position": { "x": 1125, "y": 670 },
+              "type": "Output",
+              "data": { "name": "y_test" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 1125, "y": 670 },
+              "dragging": false
+            },
+            {
+              "id": "Output_2707",
+              "position": { "x": 1285, "y": 150 },
+              "type": "Output",
+              "data": { "name": "x_test" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 1285, "y": 150 },
+              "dragging": false
+            },
+            {
+              "id": "Output_3326",
+              "position": { "x": 1290, "y": -95 },
+              "type": "Output",
+              "data": { "name": "x_train" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 1290, "y": -95 },
+              "dragging": false
+            },
+            {
+              "id": "Output_2844",
+              "position": { "x": 1125, "y": 510 },
+              "type": "Output",
+              "data": { "name": "y_train" },
+              "width": 153,
+              "height": 100,
+              "selected": false,
+              "positionAbsolute": { "x": 1125, "y": 510 },
+              "dragging": false
+            },
+            {
+              "id": "FunctionNode_RawData_3720",
+              "position": { "x": 160, "y": -65 },
+              "type": "FunctionNode/RawData",
+              "data": {
+                "name": "Emit Raw Data",
+                "hyperparams": [{ "id": "raw-python-data", "value": "None" }],
+                "commentText": "",
+                "commentType": "plain",
+                "elementID": "RawData",
+                "innerCode": "255.0"
+              },
+              "width": 180,
+              "height": 120,
+              "selected": false,
+              "positionAbsolute": { "x": 160, "y": -65 },
+              "dragging": false
+            }
+          ],
+          "edges": [
+            {
+              "id": "FunctionNode_ScalarDivide_2003_dividend",
+              "source": "FunctionNode_LoadMNISTDataset_6724",
+              "sourceHandle": "x_train",
+              "target": "FunctionNode_ScalarDivide_2003",
+              "targetHandle": "dividend",
+              "label": "x_train",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "FunctionNode_ScalarDivide_7702_dividend",
+              "source": "FunctionNode_LoadMNISTDataset_6724",
+              "sourceHandle": "x_test",
+              "target": "FunctionNode_ScalarDivide_7702",
+              "targetHandle": "dividend",
+              "label": "x_test",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "Output_2844_in",
+              "source": "FunctionNode_LoadMNISTDataset_6724",
+              "sourceHandle": "y_train",
+              "target": "Output_2844",
+              "targetHandle": "in",
+              "label": "y_train",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "Output_2250_in",
+              "source": "FunctionNode_LoadMNISTDataset_6724",
+              "sourceHandle": "y_test",
+              "target": "Output_2250",
+              "targetHandle": "in",
+              "label": "y_test",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "Output_3326_in",
+              "source": "FunctionNode_ScalarDivide_2003",
+              "sourceHandle": "result",
+              "target": "Output_3326",
+              "targetHandle": "in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "Output_2707_in",
+              "source": "FunctionNode_ScalarDivide_7702",
+              "sourceHandle": "result",
+              "target": "Output_2707",
+              "targetHandle": "in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "FunctionNode_ScalarDivide_2003_divisor",
+              "source": "FunctionNode_RawData_3720",
+              "sourceHandle": "out",
+              "target": "FunctionNode_ScalarDivide_2003",
+              "targetHandle": "divisor",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "FunctionNode_ScalarDivide_7702_divisor",
+              "source": "FunctionNode_RawData_3720",
+              "sourceHandle": "out",
+              "target": "FunctionNode_ScalarDivide_7702",
+              "targetHandle": "divisor",
+              "label": ""
+            }
+          ]
         },
-        isFolder: false,
-        children: [],
+        "isFolder": false,
+        "children": []
       },
       "/Demo Project/tuner/ModelTuner.dc": {
-        index: "/Demo Project/tuner/ModelTuner.dc",
-        data: {
-          name: "ModelTuner.dc",
-          folder: false,
-          artefact: false,
-          unsaved: true,
-          filetype: "dc",
-          artefacttype: "ModelTunerArtefact",
-          nodes: [],
-          edges: [],
+        "index": "/Demo Project/tuner/ModelTuner.dc",
+        "data": {
+          "name": "ModelTuner.dc",
+          "folder": false,
+          "artefact": false,
+          "unsaved": true,
+          "filetype": "dc",
+          "artefacttype": "ModelTunerArtefact",
+          "nodes": [
+            {
+              "id": "FunctionNode_LoadMNISTDataset_9379",
+              "position": { "x": -130, "y": 185 },
+              "type": "FunctionNode",
+              "data": {
+                "name": "Load MNIST Dataset",
+                "hyperparams": [],
+                "commentText": "",
+                "commentType": "plain",
+                "inputHandles": [],
+                "outputHandles": ["x_train", "y_train", "x_test", "y_test"],
+                "elementID": "LoadMNISTDataset"
+              },
+              "width": 353,
+              "height": 203,
+              "selected": false,
+              "positionAbsolute": { "x": -130, "y": 185 },
+              "dragging": false
+            },
+            {
+              "id": "FunctionNode_GetBestModelAfterTuning_6214",
+              "position": { "x": 605, "y": 105 },
+              "type": "FunctionNode",
+              "data": {
+                "name": "Get Best Model After Tuning",
+                "hyperparams": [
+                  { "id": "objective", "value": "'val_loss'" },
+                  { "id": "max_trials", "value": "5" },
+                  { "id": "epochs", "value": "5" }
+                ],
+                "commentText": "",
+                "commentType": "plain",
+                "inputHandles": ["build_model"],
+                "outputHandles": ["best-model"],
+                "elementID": "GetBestModelAfterTuning"
+              },
+              "width": 353,
+              "height": 203,
+              "selected": false,
+              "positionAbsolute": { "x": 605, "y": 105 },
+              "dragging": false
+            },
+            {
+              "id": "Output_1475",
+              "position": { "x": 1075, "y": 165 },
+              "type": "Output",
+              "data": { "name": "model" },
+              "width": 153,
+              "height": 100,
+              "selected": true,
+              "positionAbsolute": { "x": 1075, "y": 165 },
+              "dragging": false
+            },
+            {
+              "id": "CallBackNode_9523",
+              "position": { "x": 10, "y": 40 },
+              "data": { "name": "Compiled Classifier Model" },
+              "type": "Callback",
+              "width": 50,
+              "height": 40,
+              "selected": false,
+              "positionAbsolute": { "x": 10, "y": 40 },
+              "dragging": false
+            },
+            {
+              "id": "PackerNode_9523",
+              "position": { "x": 410, "y": 245 },
+              "type": "Packer/Named",
+              "data": {
+                "hyperparams": [{ "id": "packingCount", "value": 2 }],
+                "commentText": "",
+                "commentType": "plain"
+              },
+              "width": 50,
+              "height": 40,
+              "selected": false,
+              "positionAbsolute": { "x": 410, "y": 245 },
+              "dragging": false
+            }
+          ],
+          "edges": [
+            {
+              "id": "FunctionNode_GetBestModelAfterTuning_6214_build_model",
+              "source": "CallBackNode_9523",
+              "sourceHandle": "in",
+              "target": "FunctionNode_GetBestModelAfterTuning_6214",
+              "targetHandle": "build_model",
+              "label": "build_model",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "PackerNode_9523_multi-in+5023",
+              "source": "FunctionNode_LoadMNISTDataset_9379",
+              "sourceHandle": "x_train",
+              "target": "PackerNode_9523",
+              "targetHandle": "multi-in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "PackerNode_9523_multi-in+304",
+              "source": "FunctionNode_LoadMNISTDataset_9379",
+              "sourceHandle": "y_train",
+              "target": "PackerNode_9523",
+              "targetHandle": "multi-in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "PackerNode_9523_multi-in+3929",
+              "source": "FunctionNode_LoadMNISTDataset_9379",
+              "sourceHandle": "x_test",
+              "target": "PackerNode_9523",
+              "targetHandle": "multi-in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "PackerNode_9523_multi-in+8146",
+              "source": "FunctionNode_LoadMNISTDataset_9379",
+              "sourceHandle": "y_test",
+              "target": "PackerNode_9523",
+              "targetHandle": "multi-in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "FunctionNode_GetBestModelAfterTuning_6214_multi-in+8878",
+              "source": "PackerNode_9523",
+              "sourceHandle": "out",
+              "target": "FunctionNode_GetBestModelAfterTuning_6214",
+              "targetHandle": "multi-in",
+              "label": "",
+              "animated": true,
+              "style": { "stroke": "#000", "strokeWidth": 1.5 }
+            },
+            {
+              "id": "Output_1475_in",
+              "source": "FunctionNode_GetBestModelAfterTuning_6214",
+              "sourceHandle": "best-model",
+              "target": "Output_1475",
+              "targetHandle": "in",
+              "label": ""
+            }
+          ]
         },
-        isFolder: false,
-        children: [],
+        "isFolder": false,
+        "children": []
       },
       "/Demo Project/tuner/ModelEvaluator.dc": {
         index: "/Demo Project/tuner/ModelEvaluator.dc",
